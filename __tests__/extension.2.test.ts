4e7f1a8134af55a643096d6971cbcd40eabe4f57
Wright, Christopher R <Christopher.Wright@ca.com>
2018-10-11 13:58:21 -0400

    Add basic imports
    Signed-off-by: Wright, Christopher R <Christopher.Wright@ca.com>

--- __tests__/extension.2.test.ts
+++ __tests__/extension.2.test.ts
@@ -1,3 +1,7 @@
+import * as vscode from "vscode";
+import * as path from "path";
+import * as extension from "../src/extension";
+
 describe("tests moved over", () => {
   it("Testing that activate correctly executes", () => {
     pending("Ly");
