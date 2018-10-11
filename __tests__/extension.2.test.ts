7ffba5ef0db34e889587dce7131eec7a677a9df7
Ly,Lam <lyzla01@ca.com>
2018-10-11 16:49:01 -0400

    temp commit
    Signed-off-by: Ly,Lam <lyzla01@ca.com>

--- __tests__/extension.2.test.ts
+++ __tests__/extension.2.test.ts
@@ -1,11 +1,37 @@
+jest.mock("fs");
+jest.mock("vscode");
+jest.mock("../src/DatasetTree");
 import * as vscode from "vscode";
 import * as path from "path";
+import * as fs from "fs";
 import * as extension from "../src/extension";
+import { DatasetTree } from "../src/DatasetTree";
+import { activate, deactivate } from "../src/extension"; 
+
+(fs as any).mkdirSync = jest.fn();
 
 describe("tests moved over", () => {
   fdescribe("activate should", () => {
+    const extensionMock = jest.fn<vscode.ExtensionContext>(() => ({
+      subscriptions: []
+    }));
+    const mock = new extensionMock();
+
+    beforeEach(() => {
+      (DatasetTree as any).addSession = jest.fn();
+      (fs as any).existsSync = jest.fn().mockResolvedValueOnce(false);
+      (extension as any).initializeFavorites = jest.fn();
+    });
+
+    afterEach(() => {
+
+    });
+
     it("should execute correctly", async () => {
       console.log("start testing");
+      await extension.activate(mock);
+
+
     });
   });
 
