a2fbd8e785daf806c52e05d96b4b88554e9358bf
Wright, Christopher R <Christopher.Wright@ca.com>
2018-10-11 13:50:43 -0400

    Break file out
    Signed-off-by: Wright, Christopher R <Christopher.Wright@ca.com>

---
+++ __tests__/extension.2.test.ts
@@ -0,0 +1,9 @@
+describe("tests moved over", () => {
+  it("Testing that activate correctly executes", () => {
+    pending("Ly");
+  });
+
+  it("Testing that saveFile is executed successfully", () => {
+    pending("Chris");
+  });
+});
