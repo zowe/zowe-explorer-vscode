33cad94758a05f913980775f1f8d340a5676accb
KRISTINA MAYO <ktopchi@us.ibm.com>
2019-04-16 11:26:59 -0400

    Unit test createUSSNode function
    Signed-off-by: KRISTINA MAYO <ktopchi@us.ibm.com>
    Signed-off-by: boech02 <christopher.boehm@broadcom.com>

--- src/extension.ts
+++ src/extension.ts
@@ -121,39 +121,6 @@ export async function activate(context: vscode.ExtensionContext) {
 }
 
 /**
- * Prompts the user for a path, and populates the [TreeView]{@link vscode.TreeView} based on the path
- *
- * @param {ZoweUSSNode} node - The session node
- * @param {ussTree} ussFileProvider - Current ussTree used to populate the TreeView
- * @returns {Promise<void>}
- */
-export async function createUSSNode(node: ZoweUSSNode, ussFileProvider: USSTree, nodeType: string) {
-    const name = await vscode.window.showInputBox({placeHolder: "Name of Member"});
-    if (name) {
-        try {
-            const filePath = `${node.fullPath}/${name}`;
-            await zowe.Create.uss(node.getSession(), filePath, nodeType);
-            ussFileProvider.refresh();
-        } catch (err) {
-            vscode.window.showErrorMessage(`Unable to create node: ${err.message}`);
-            throw (err);
-        }
-    }
-}
-
-export async function deleteUSSNode(node: ZoweUSSNode, ussFileProvider: USSTree) {
-    const nodePath = node.fullPath;
-    try {
-        const isRecursive = node.contextValue === "directory" ? true : false;
-        await zowe.Delete.ussFile(node.getSession(), nodePath, isRecursive);
-        ussFileProvider.refresh();
-    } catch (err) {
-        vscode.window.showErrorMessage(`Unable to delete node: ${err.message}`);
-        throw (err);
-    }
-}
-
-/**
  * Submit the contents of the editor as JCL. 
  * 
  * @export
@@ -1045,6 +1012,39 @@ export async function openUSS(node: ZoweUSSNode) {
     } catch (err) {
         log.error("Error encountered when opening USS file: " + JSON.stringify(err));
         vscode.window.showErrorMessage(err.message);
+        throw (err);
+    }
+}
+
+/**
+ * Prompts the user for a path, and populates the [TreeView]{@link vscode.TreeView} based on the path
+ *
+ * @param {ZoweUSSNode} node - The session node
+ * @param {ussTree} ussFileProvider - Current ussTree used to populate the TreeView
+ * @returns {Promise<void>}
+ */
+export async function createUSSNode(node: ZoweUSSNode, ussFileProvider: USSTree, nodeType: string) {
+    const name = await vscode.window.showInputBox({placeHolder: "Name of Member"});
+    if (name) {
+        try {
+            const filePath = `${node.fullPath}/${name}`;
+            await zowe.Create.uss(node.getSession(), filePath, nodeType);
+            ussFileProvider.refresh();
+        } catch (err) {
+            vscode.window.showErrorMessage(`Unable to create node: ${err.message}`);
+            throw (err);
+        }
+    }
+}
+
+export async function deleteUSSNode(node: ZoweUSSNode, ussFileProvider: USSTree) {
+    const nodePath = node.fullPath;
+    try {
+        const isRecursive = node.contextValue === "directory" ? true : false;
+        await zowe.Delete.ussFile(node.getSession(), nodePath, isRecursive);
+        ussFileProvider.refresh();
+    } catch (err) {
+        vscode.window.showErrorMessage(`Unable to delete node: ${err.message}`);
         throw (err);
     }
 }
