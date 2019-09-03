d3e4c418b48f518331c19b1ea3cacf7f759bdd26
Lauren Li <Lauren.Li@ibm.com>
2019-09-03 18:08:55 -0400

    Update i18n files and add info message for Theia Copy Path
    Signed-off-by: Lauren Li <Lauren.Li@ibm.com>

--- src/uss/ussNodeActions.ts
+++ src/uss/ussNodeActions.ts
@@ -20,6 +20,7 @@ const localize = nls.config({ messageFormat: nls.MessageFormat.file })();
 
 import * as path from "path";
 import { loadNamedProfile } from "../ProfileLoader";
+import { ISTHEIA } from "../extension";
 /**
  * Prompts the user for a path, and populates the [TreeView]{@link vscode.TreeView} based on the path
  *
@@ -172,5 +173,10 @@ export async function uploadFile(node: ZoweUSSNode, doc: vscode.TextDocument) {
  * @param {ZoweUSSNode} node
  */
 export async function copyPath(node: ZoweUSSNode) {
+    if (ISTHEIA) {
+        // Remove when Theia supports VS Code API for accessing system clipboard
+        vscode.window.showInformationMessage(localize("copyPath.infoMessage", "Copy Path is not yet supported in Theia."));
+        return;
+    }
     vscode.env.clipboard.writeText(node.fullPath);
 }
