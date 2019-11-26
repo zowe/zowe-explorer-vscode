66a0d8f33d9421412303c72be6102882f62089e7
Lauren Li <Lauren.Li@ibm.com>
2019-11-25 20:48:28 -0500

    Update previous temp folder path for webpack
    Signed-off-by: Lauren Li <Lauren.Li@ibm.com>

--- src/extension.ts
+++ src/extension.ts
@@ -367,7 +367,7 @@ export function moveTempFolder(previousTempPath: string, currentTempPath: string
     defineGlobals(currentTempPath);
 
     if (previousTempPath === "") {
-        previousTempPath = path.join(__dirname, "..", "..", "resources");
+        previousTempPath = path.join(__dirname, "..", "resources");
     }
 
     // Make certain that "temp" folder is cleared
