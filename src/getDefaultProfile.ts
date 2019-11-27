302920e9dff1ce2a9fffa1bc00729ddbc747a8ff
zFernand0 <fernando.rijocedeno@broadcom.com>
2019-11-27 09:11:53 -0500

    Simplify redentialManagerFactory initialization
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- src/getDefaultProfile.ts
+++ src/getDefaultProfile.ts
@@ -9,7 +9,7 @@
 *                                                                                 *
 */
 
-import { Imperative, CliProfileManager, ImperativeConfig } from "@zowe/imperative";
+import { Imperative, CliProfileManager, ImperativeConfig, CredentialManagerFactory, ImperativeError } from "@zowe/imperative";
 import * as path from "path";
 
 /**
@@ -21,6 +21,22 @@ import * as path from "path";
     // we have to mock a few things to get the Imperative.init to work properly
     (process.mainModule as any).filename = require.resolve("@zowe/cli");
     ((process.mainModule as any).paths as any).unshift(mainZoweDir);
+
+    // We need the credential manager factory to allow different instances of the AbstractCredentialManager
+    // @brightside/imperative vs. @zowe/imperative
+    const originalFactoryInitialize = CredentialManagerFactory.initialize;
+    ((CredentialManagerFactory as any).initialize as any) = async (params: any): Promise<void> => {
+      try {
+        const displayName = (params.displayName == null) ? params.service : params.displayName;
+        (CredentialManagerFactory as any).mManager = new params.Manager(params.service, displayName);
+        if ((CredentialManagerFactory as any).mManager.initialize) {
+          await (CredentialManagerFactory as any).mManager.initialize();
+        }
+      } catch (err) {
+        throw new ImperativeError({msg: err.toString()});
+      }
+    };
+
     // we need to call Imperative.init so that any installed credential manager plugins are loaded
     await Imperative.init({ configurationModule: require.resolve("@zowe/cli/lib/imperative.js") });
     const zosmfProfiles = await new CliProfileManager({
@@ -29,4 +45,7 @@ import * as path from "path";
     }).load({ loadDefault: true });
 
     process.stdout.write(JSON.stringify(zosmfProfiles, null, 2));
+
+    // Revert back to the original implementation
+    ((CredentialManagerFactory as any).initialize as any) = originalFactoryInitialize;
 })();
