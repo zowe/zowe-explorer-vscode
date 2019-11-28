e0a5444c8095f43c6399e88c542c448b06d9fcfc
Richelle Anne Craw <richelleanne.craw@broadcom.com>
2019-11-28 23:14:27 +0100

    test: add more unit tests for favorites route, fix lint issues
    Signed-off-by: Richelle Anne Craw <richelleanne.craw@broadcom.com>

--- __tests__/__unit__/extension.unit.test.ts
+++ __tests__/__unit__/extension.unit.test.ts
@@ -1234,6 +1234,67 @@ describe("Extension Unit Tests", () => {
         expect(dataSetCreate.mock.calls.length).toBe(5);
     });
 
+    it("tests the createFile for prompt credentials, favorite route", async () => {
+        Object.defineProperty(profileLoader.Profiles, "getInstance", {
+            value: jest.fn(() => {
+                return {
+                    allProfiles: [{name: "firstName", profile: {user:undefined, password: undefined}}, {name: "secondName"}],
+                    defaultProfile: {name: "firstName"},
+                    promptCredentials: jest.fn(()=> {
+                        return [{values: "fake"}, {values: "fake"}, {values: "fake"}];
+                    }),
+                };
+            })
+        });
+        const sessionwocred = new brtimperative.Session({
+            user: "",
+            password: "",
+            hostname: "fake",
+            protocol: "https",
+            type: "basic",
+        });
+
+        createBasicZosmfSession.mockReturnValue(sessionwocred);
+        const newsessNode = new ZoweNode("sestest", vscode.TreeItemCollapsibleState.Expanded, null, sessionwocred);
+        newsessNode.contextValue = extension.DS_SESSION_CONTEXT + extension.FAV_SUFFIX;
+
+        showQuickPick.mockReset();
+        getConfiguration.mockReset();
+        showInputBox.mockReset();
+        dataSetCreate.mockReset();
+
+        getConfiguration.mockReturnValue("FakeConfig");
+        showInputBox.mockReturnValueOnce("FakeName");
+
+
+        showQuickPick.mockResolvedValueOnce("Data Set Binary");
+        await extension.createFile(newsessNode, testTree);
+        showQuickPick.mockResolvedValueOnce("Data Set C");
+        await extension.createFile(newsessNode, testTree);
+        showQuickPick.mockResolvedValueOnce("Data Set Classic");
+        await extension.createFile(newsessNode, testTree);
+        showQuickPick.mockResolvedValueOnce("Data Set Partitioned");
+        await extension.createFile(newsessNode, testTree);
+        showQuickPick.mockResolvedValueOnce("Data Set Sequential");
+        await extension.createFile(newsessNode, testTree);
+
+        // tslint:disable-next-line: no-magic-numbers
+        expect(showQuickPick.mock.calls.length).toBe(5);
+        // tslint:disable-next-line: no-magic-numbers
+        expect(getConfiguration.mock.calls.length).toBe(5);
+        expect(getConfiguration.mock.calls[0][0]).toBe("Zowe-Default-Datasets-Binary");
+        expect(getConfiguration.mock.calls[1][0]).toBe("Zowe-Default-Datasets-C");
+        expect(getConfiguration.mock.calls[2][0]).toBe("Zowe-Default-Datasets-Classic");
+        // tslint:disable-next-line: no-magic-numbers
+        expect(getConfiguration.mock.calls[3][0]).toBe("Zowe-Default-Datasets-PDS");
+        // tslint:disable-next-line: no-magic-numbers
+        expect(getConfiguration.mock.calls[4][0]).toBe("Zowe-Default-Datasets-PS");
+        // tslint:disable-next-line: no-magic-numbers
+        expect(showInputBox.mock.calls.length).toBe(5);
+        // tslint:disable-next-line: no-magic-numbers
+        expect(dataSetCreate.mock.calls.length).toBe(5);
+    });
+
     it("tests the createFile for prompt credentials error", async () => {
         Object.defineProperty(profileLoader.Profiles, "getInstance", {
             value: jest.fn(() => {
@@ -1674,8 +1735,6 @@ describe("Extension Unit Tests", () => {
             protocol: "https",
             type: "basic",
         });
-        const sessNode = new ZoweNode("sestest", vscode.TreeItemCollapsibleState.Expanded, null, session);
-        sessNode.contextValue = extension.DS_SESSION_CONTEXT;
         const dsNode = new ZoweNode("testSess", vscode.TreeItemCollapsibleState.Expanded, sessNode, sessionwocred);
         dsNode.contextValue = extension.DS_SESSION_CONTEXT;
         Object.defineProperty(profileLoader.Profiles, "getInstance", {
@@ -1711,8 +1770,6 @@ describe("Extension Unit Tests", () => {
             protocol: "https",
             type: "basic",
         });
-        const sessNode = new ZoweNode("sestest", vscode.TreeItemCollapsibleState.Expanded, null, session);
-        sessNode.contextValue = extension.FAVORITE_CONTEXT;
         const dsNode = new ZoweNode("[test]: TEST.JCL", vscode.TreeItemCollapsibleState.Expanded, sessNode, sessionwocred);
         dsNode.contextValue = extension.DS_PDS_CONTEXT + extension.FAV_SUFFIX;
         Object.defineProperty(profileLoader.Profiles, "getInstance", {
@@ -1748,8 +1805,6 @@ describe("Extension Unit Tests", () => {
             protocol: "https",
             type: "basic",
         });
-        const sessNode = new ZoweNode("sestest", vscode.TreeItemCollapsibleState.Expanded, null, session);
-        sessNode.contextValue = extension.DS_SESSION_CONTEXT;
         const dsNode = new ZoweNode("testSess", vscode.TreeItemCollapsibleState.Expanded, sessNode, sessionwocred);
         dsNode.contextValue = extension.DS_SESSION_CONTEXT;
         Object.defineProperty(profileLoader.Profiles, "getInstance", {
@@ -2409,9 +2464,7 @@ describe("Extension Unit Tests", () => {
             protocol: "https",
             type: "basic",
         });
-        const sessNode = new ZoweUSSNode("sestest", vscode.TreeItemCollapsibleState.Expanded, null, session, null);
-        sessNode.contextValue = extension.USS_SESSION_CONTEXT;
-        const dsNode = new ZoweUSSNode("testSess", vscode.TreeItemCollapsibleState.Expanded, sessNode, sessionwocred, null);
+        const dsNode = new ZoweUSSNode("testSess", vscode.TreeItemCollapsibleState.Expanded, ussNode, sessionwocred, null);
         dsNode.contextValue = extension.USS_SESSION_CONTEXT;
         Object.defineProperty(profileLoader.Profiles, "getInstance", {
             value: jest.fn(() => {
@@ -2446,9 +2499,7 @@ describe("Extension Unit Tests", () => {
             protocol: "https",
             type: "basic",
         });
-        const sessNode = new ZoweUSSNode("sestest", vscode.TreeItemCollapsibleState.Expanded, null, session, null);
-        sessNode.contextValue = extension.USS_SESSION_CONTEXT;
-        const dsNode = new ZoweUSSNode("testSess", vscode.TreeItemCollapsibleState.Expanded, sessNode, sessionwocred, null);
+        const dsNode = new ZoweUSSNode("testSess", vscode.TreeItemCollapsibleState.Expanded, ussNode, sessionwocred, null);
         dsNode.contextValue = extension.USS_DIR_CONTEXT + extension.FAV_SUFFIX;
         Object.defineProperty(profileLoader.Profiles, "getInstance", {
             value: jest.fn(() => {
@@ -2483,9 +2534,7 @@ describe("Extension Unit Tests", () => {
             protocol: "https",
             type: "basic",
         });
-        const sessNode = new ZoweUSSNode("sestest", vscode.TreeItemCollapsibleState.Expanded, null, session, null);
-        sessNode.contextValue = extension.USS_SESSION_CONTEXT;
-        const dsNode = new ZoweUSSNode("testSess", vscode.TreeItemCollapsibleState.Expanded, sessNode, sessionwocred, null);
+        const dsNode = new ZoweUSSNode("testSess", vscode.TreeItemCollapsibleState.Expanded, ussNode, sessionwocred, null);
         dsNode.contextValue = extension.USS_SESSION_CONTEXT;
         Object.defineProperty(profileLoader.Profiles, "getInstance", {
             value: jest.fn(() => {
@@ -2669,6 +2718,35 @@ describe("Extension Unit Tests", () => {
             const newjobNode = new Job("jobtest", vscode.TreeItemCollapsibleState.Expanded, jobNode, sessionwocred, iJob);
             newjobNode.contextValue = "server";
             newjobNode.contextValue = "server";
+            await extension.refreshJobsServer(newjobNode);
+            expect(extension.refreshJobsServer).toHaveBeenCalled();
+        });
+
+        it("tests the refresh Jobs Server for prompt credentials, favorites route", async () => {
+            showQuickPick.mockReset();
+            showInputBox.mockReset();
+            const addJobsSession = jest.spyOn(extension, "refreshJobsServer");
+            Object.defineProperty(profileLoader.Profiles, "getInstance", {
+                value: jest.fn(() => {
+                    return {
+                        allProfiles: [{name: "firstName", profile: {user:undefined, password: undefined}}, {name: "secondName"}],
+                        defaultProfile: {name: "firstName"},
+                        promptCredentials: jest.fn(()=> {
+                            return [{values: "fake"}, {values: "fake"}, {values: "fake"}];
+                        }),
+                    };
+                })
+            });
+            const sessionwocred = new brtimperative.Session({
+                user: "",
+                password: "",
+                hostname: "fake",
+                protocol: "https",
+                type: "basic",
+            });
+            createBasicZosmfSession.mockReturnValue(sessionwocred);
+            const newjobNode = new Job("jobtest", vscode.TreeItemCollapsibleState.Expanded, jobNode, sessionwocred, iJob);
+            newjobNode.contextValue = extension.JOBS_SESSION_CONTEXT + extension.FAV_SUFFIX;
             await extension.refreshJobsServer(newjobNode);
             expect(extension.refreshJobsServer).toHaveBeenCalled();
         });
