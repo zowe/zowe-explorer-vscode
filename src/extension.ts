120a07bd9da25c1157891a0223af45d27d9ecc1b
Colin Stone <30794003+Colin-Stone@users.noreply.github.com>
2020-01-23 14:24:31 +0000

    Add warning message if unable to load keytar
    Signed-off-by: Colin Stone <30794003+Colin-Stone@users.noreply.github.com>

--- src/extension.ts
+++ src/extension.ts
@@ -361,12 +361,15 @@ export function getSecurityModules(moduleName): NodeRequire | undefined {
     }
     if (imperativeIsSsecure) {
         try {
-            return require(`${vscode.env.appRoot}/node_modules.asar/${moduleName}`);
-            // tslint:disable-next-line: no-empty
-        } catch (err) {}
-        try {
             return require(`${vscode.env.appRoot}/node_modules/${moduleName}`);
         // tslint:disable-next-line: no-empty
+        } catch (err) {
+            vscode.window.showWarningMessage(localize("initialize.module.load",
+                    "Credentials not managed, unable to load security file: ") + moduleName);
+        }
+        try {
+            return require(`${vscode.env.appRoot}/node_modules.asar/${moduleName}`);
+            // tslint:disable-next-line: no-empty
         } catch (err) {}
     }
     return undefined;
