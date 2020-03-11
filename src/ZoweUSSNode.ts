920512cd23a6b79ba6305e2ba9b2fd5e5eefc698
Peter Haumer <4391934+phaumer@users.noreply.github.com>
2020-03-10 18:29:01 -0700

    Fixed changed VSC behavior for tree expand
    Signed-off-by: Peter Haumer <4391934+phaumer@users.noreply.github.com>

--- src/ZoweUSSNode.ts
+++ src/ZoweUSSNode.ts
@@ -38,7 +38,7 @@ import { attachRecentSaveListener, disposeRecentSaveListener, getRecentSaveStatu
 export class ZoweUSSNode extends ZoweTreeNode implements IZoweUSSTreeNode {
     public command: vscode.Command;
     public fullPath = "";
-    public dirty = extension.ISTHEIA;  // Make sure this is true for theia instances
+    public dirty = true;
     public children: IZoweUSSTreeNode[] = [];
     public binaryFiles = {};
     public binary = false;
