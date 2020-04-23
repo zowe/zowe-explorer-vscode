0ccd2b5a215c5bdea6a433673838d9d794d00d2e
Katelyn Nienaber <katelyn.nienaber@broadcom.com>
2020-04-23 10:29:52 +0200

    WIP
    Signed-off-by: Katelyn Nienaber <katelyn.nienaber@broadcom.com>

---
+++ __tests__/__unit__/uss/USSTree.unit.test.ts
@@ -0,0 +1,24 @@
+/*
+* This program and the accompanying materials are made available under the terms of the *
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at *
+* https://www.eclipse.org/legal/epl-v20.html                                      *
+*                                                                                 *
+* SPDX-License-Identifier: EPL-2.0                                                *
+*                                                                                 *
+* Copyright Contributors to the Zowe Project.                                     *
+*                                                                                 *
+*/
+
+describe("USSTree Unit Tests - Function USSTree.deleteSession()", () => {
+
+    beforeEach(() => {
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("Tests if deleteSession is executed successfully", async () => {
+
+    });
+});
