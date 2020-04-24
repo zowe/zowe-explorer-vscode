a90d378ccb085caf325eb6612499f64dc98ff7a9
Katelyn Nienaber <katelyn.nienaber@broadcom.com>
2020-04-24 15:05:47 +0200

    Moved all tests for USS node-related actions functions into __tests__/__unit__/uss/USSTree.unit.test.ts
    Signed-off-by: Katelyn Nienaber <katelyn.nienaber@broadcom.com>

--- __mocks__/generators/uss.ts
+++ __mocks__/generators/uss.ts
@@ -11,11 +11,7 @@
 
 import { ZoweUSSNode } from "../../src/uss/ZoweUSSNode";
 import * as vscode from "vscode";
-<<<<<<< HEAD
 import * as globals from "../../src/globals";
-=======
-import { USS_SESSION_CONTEXT, FAVORITE_CONTEXT, DS_TEXT_FILE_CONTEXT, FAV_SUFFIX } from "../../src/globals";
->>>>>>> WIP
 import { getIconByNode } from "../../src/generators/icons";
 
 const mockUSSRefresh = jest.fn();
@@ -61,17 +57,10 @@ export function generateFavoriteUSSNode(session, profile) {
 }
 
 export function addSessionNode(theTree, theSession, theProfile) {
-<<<<<<< HEAD
     const newSessNode = new ZoweUSSNode("ussTestSess", vscode.TreeItemCollapsibleState.Collapsed, null, theSession, null, false, theProfile.name);
     theTree.mSessionNodes.push(newSessNode);
     const sessionIndex = theTree.mSessionNodes.length - 1;
     theTree.mSessionNodes[sessionIndex].contextValue = globals.USS_SESSION_CONTEXT;
-=======
-    const newSessNode = new ZoweUSSNode("ussTestSess", vscode.TreeItemCollapsibleState.Collapsed, null, theSession, null, false, theProfile.name)
-    theTree.mSessionNodes.push(newSessNode);
-    const sessionIndex = theTree.mSessionNodes.length - 1;
-    theTree.mSessionNodes[sessionIndex].contextValue = USS_SESSION_CONTEXT;
->>>>>>> WIP
     theTree.mSessionNodes[sessionIndex].fullPath = "test";
     const targetIcon = getIconByNode(theTree.mSessionNodes[sessionIndex]);
     if (targetIcon) {
