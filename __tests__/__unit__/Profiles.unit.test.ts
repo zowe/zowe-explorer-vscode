7b9f38603d4e14c516ada077606b71ca5f71bf94
Peter Haumer <4391934+phaumer@users.noreply.github.com>
2020-04-24 18:34:38 -0700

    Removed unused imports and consts
    Signed-off-by: Peter Haumer <4391934+phaumer@users.noreply.github.com>

--- __tests__/__unit__/Profiles.unit.test.ts
+++ __tests__/__unit__/Profiles.unit.test.ts
@@ -11,14 +11,11 @@
 
 jest.mock("vscode");
 jest.mock("child_process");
-import * as path from "path";
-import * as os from "os";
 import * as vscode from "vscode";
 import * as child_process from "child_process";
 import { Logger, IProfileLoaded, CliProfileManager } from "@zowe/imperative";
-import * as testConst from "../../resources/testProfileData";
 import { Profiles, ValidProfileEnum } from "../../src/Profiles";
-import { ZosmfSession, getImperativeConfig } from "@zowe/cli";
+import { ZosmfSession } from "@zowe/cli";
 import { ZoweExplorerApiRegister } from "../../src/api/ZoweExplorerApiRegister";
 
 describe("Profile class unit tests", () => {
@@ -27,27 +24,7 @@ describe("Profile class unit tests", () => {
 
     const profileOne = { name: "profile1", profile: {}, type: "zosmf" };
     const profileTwo = { name: "profile2", profile: {}, type: "zosmf" };
-    const inputBox: vscode.InputBox = {
-        value: "input",
-        title: null,
-        enabled: true,
-        busy: false,
-        show: jest.fn(),
-        hide: jest.fn(),
-        step: null,
-        dispose: jest.fn(),
-        ignoreFocusOut: false,
-        totalSteps: null,
-        placeholder: undefined,
-        password: false,
-        onDidChangeValue: jest.fn(),
-        onDidAccept: jest.fn(),
-        onDidHide: jest.fn(),
-        buttons: [],
-        onDidTriggerButton: jest.fn(),
-        prompt: undefined,
-        validationMessage: undefined
-    };
+
     const schema: {} = {
         host:{type:"string",optionDefinition:{description:"description"}},
         port:{type:"number",optionDefinition:{description:"description", defaultValue: 443}},
@@ -80,7 +57,6 @@ describe("Profile class unit tests", () => {
     // tslint:disable-next-line:max-line-length
     const cliProfileManagerMock = Object.create({configurations:[{type:"zosmf",schema:{type:"object",title:"z\/OSMF Profile",description:"z\/OSMF Profile",properties:{host:{type:"string",optionDefinition:{name:"host",aliases:["H"],description:"The z\/OSMF server host name.",type:"string",required:true,group:"Zosmf Connection Options"}},port:{type:"number",optionDefinition:{name:"port",aliases:["P"],description:"The z\/OSMF server port.",type:"number",defaultValue:443,group:"Zosmf Connection Options"}},user:{type:"string",secure:true,optionDefinition:{name:"user",aliases:["u"],description:"Mainframe (z\/OSMF) user name, which can be the same as your TSO login.",type:"string",required:true,group:"Zosmf Connection Options"}},password:{type:"string",secure:true,optionDefinition:{name:"password",aliases:["pass","pw"],description:"Mainframe (z\/OSMF) password, which can be the same as your TSO password.",type:"string",group:"Zosmf Connection Options",required:true}},rejectUnauthorized:{type:"boolean",optionDefinition:{name:"reject-unauthorized",aliases:["ru"],description:"Reject self-signed certificates.",type:"boolean",defaultValue:true,group:"Zosmf Connection Options"}},basePath:{type:"string",optionDefinition:{name:"base-path",aliases:["bp"],description:"The base path for your API mediation layer instance. Specify this option to prepend the base path to all z\/OSMF resources when making REST requests. Do not specify this option if you are not using an API mediation layer.",type:"string",group:"Zosmf Connection Options"}}},required:["host"]},createProfileExamples:[{options:"zos123 --host zos123 --port 1443 --user ibmuser --password myp4ss",description:"Create a zosmf profile called 'zos123' to connect to z\/OSMF at host zos123 and port 1443"},{options:"zos124 --host zos124 --user ibmuser --password myp4ss --reject-unauthorized false",description:"Create a zosmf profile called 'zos124' to connect to z\/OSMF at the host zos124 (default port - 443) and allow self-signed certificates"},{options:"zosAPIML --host zosAPIML --port 2020 --user ibmuser --password myp4ss --reject-unauthorized false --base-path basePath",description:"Create a zosmf profile called 'zos124' to connect to z\/OSMF at the host zos124 (default port - 443) and allow self-signed certificates"}],updateProfileExamples:[{options:"zos123 --user newuser --password newp4ss",description:"Update a zosmf profile named 'zos123' with a new username and password"}]}]}) as CliProfileManager;
 
-    const homedir = path.join(os.homedir(), ".zowe");
     const mockJSONParse = jest.spyOn(JSON, "parse");
     const showInformationMessage = jest.fn();
     const showInputBox = jest.fn();
