cdbb8a2d8a1d3331d3e32d3874e22bdfbd6c8c97
Peter Haumer <4391934+phaumer@users.noreply.github.com>
2020-04-26 14:41:25 -0700

    Added ApiRegister tests
    Signed-off-by: Peter Haumer <4391934+phaumer@users.noreply.github.com>

---
+++ __tests__/__unit__/api/ZoweExplorerApiRegister.unit.test.ts
@@ -0,0 +1,166 @@
+/*
+* This program and the accompanying materials are made available under the terms of the *
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at *
+* https://www.eclipse.org/legal/epl-v20.html                                      *
+*                                                                                 *
+* SPDX-License-Identifier: EPL-2.0                                                *
+*                                                                                 *
+* Copyright Contributors to the Zowe Project.                                     *
+*                                                                                 *
+*/
+
+// tslint:disable: max-classes-per-file
+
+jest.mock("@zowe/imperative");
+import { Logger } from "@zowe/imperative";
+import { ZoweExplorerApi } from "../../../src/api/ZoweExplorerApi";
+import { ZoweExplorerApiRegister } from "../../../src/api/ZoweExplorerApiRegister";
+import { ZosmfUssApi, ZosmfJesApi, ZosmfMvsApi } from "../../../src/api/ZoweExplorerZosmfApi";
+import { Profiles } from "../../../src/Profiles";
+
+class MockUssApi1 implements ZoweExplorerApi.IUss {
+    public profile?: import("@zowe/imperative").IProfileLoaded;
+    public getProfileTypeName(): string {
+        return "api1typename";
+    }
+    public fileList(ussFilePath: string): Promise<import("@zowe/cli").IZosFilesResponse> {
+        throw new Error("Method not implemented.");
+    }
+    public isFileTagBinOrAscii(ussFilePath: string): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    public getContents(ussFilePath: string, options: import("@zowe/cli").IDownloadOptions): Promise<import("@zowe/cli").IZosFilesResponse> {
+        throw new Error("Method not implemented.");
+    }
+    public putContents(
+        inputFilePath: string, ussFilePath: string, binary?: boolean,
+        localEncoding?: string, etag?: string, returnEtag?: boolean): Promise<import("@zowe/cli").IZosFilesResponse> {
+        throw new Error("Method not implemented.");
+    }
+    public uploadDirectory(inputDirectoryPath: string, ussDirectoryPath: string, options: import("@zowe/cli").IUploadOptions): Promise<import("@zowe/cli").IZosFilesResponse> {
+        throw new Error("Method not implemented.");
+    }
+    public create(ussPath: string, type: string, mode?: string): Promise<import("@zowe/cli").IZosFilesResponse> {
+        throw new Error("Method not implemented.");
+    }
+    public delete(ussPath: string, recursive?: boolean): Promise<import("@zowe/cli").IZosFilesResponse> {
+        throw new Error("Method not implemented.");
+    }
+    public rename(currentUssPath: string, newUssPath: string): Promise<import("@zowe/cli").IZosFilesResponse> {
+        throw new Error("Method not implemented.");
+    }
+    public getSession(profile?: import("@zowe/imperative").IProfileLoaded): import("@zowe/imperative").Session {
+        throw new Error("Method not implemented.");
+    }
+}
+
+class MockUssApi2 implements ZoweExplorerApi.IUss {
+    public profile?: import("@zowe/imperative").IProfileLoaded;
+    public getProfileTypeName(): string {
+        return "api2typename";
+    }
+    public fileList(ussFilePath: string): Promise<import("@zowe/cli").IZosFilesResponse> {
+        throw new Error("Method not implemented.");
+    }
+    public isFileTagBinOrAscii(ussFilePath: string): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    public getContents(ussFilePath: string, options: import("@zowe/cli").IDownloadOptions): Promise<import("@zowe/cli").IZosFilesResponse> {
+        throw new Error("Method not implemented.");
+    }
+    public putContents(
+        inputFilePath: string, ussFilePath: string, binary?: boolean,
+        localEncoding?: string, etag?: string, returnEtag?: boolean): Promise<import("@zowe/cli").IZosFilesResponse> {
+        throw new Error("Method not implemented.");
+    }
+    public uploadDirectory(inputDirectoryPath: string, ussDirectoryPath: string, options: import("@zowe/cli").IUploadOptions): Promise<import("@zowe/cli").IZosFilesResponse> {
+        throw new Error("Method not implemented.");
+    }
+    public create(ussPath: string, type: string, mode?: string): Promise<import("@zowe/cli").IZosFilesResponse> {
+        throw new Error("Method not implemented.");
+    }
+    public delete(ussPath: string, recursive?: boolean): Promise<import("@zowe/cli").IZosFilesResponse> {
+        throw new Error("Method not implemented.");
+    }
+    public rename(currentUssPath: string, newUssPath: string): Promise<import("@zowe/cli").IZosFilesResponse> {
+        throw new Error("Method not implemented.");
+    }
+    public getSession(profile?: import("@zowe/imperative").IProfileLoaded): import("@zowe/imperative").Session {
+        throw new Error("Method not implemented.");
+    }
+}
+
+describe("ZoweExplorerApiRegister unit testing", () => {
+
+    const log = Logger.getAppLogger();
+    let profiles: Profiles;
+    beforeEach(async () => {
+        profiles = await Profiles.createInstance(log);
+    });
+
+    const registry = ZoweExplorerApiRegister.getInstance();
+
+    it("registers an API only once per profile type", async () => {
+        const defaultProfile = profiles.getDefaultProfile();
+
+        const defaultUssApi = registry.getUssApi(defaultProfile);
+        registry.registerUssApi(new ZosmfUssApi());
+        const anotherUssApiInstance = registry.getUssApi(defaultProfile);
+        expect(anotherUssApiInstance).toEqual(defaultUssApi);
+
+        const defaultMvsApi = registry.getMvsApi(defaultProfile);
+        registry.registerMvsApi(new ZosmfMvsApi());
+        const anotherMvsApiInstance = registry.getMvsApi(defaultProfile);
+        expect(anotherMvsApiInstance).toEqual(defaultMvsApi);
+
+        const defaultJesApi = registry.getJesApi(defaultProfile);
+        registry.registerJesApi(new ZosmfJesApi());
+        const anotherJesApiInstance = registry.getJesApi(defaultProfile);
+        expect(anotherJesApiInstance).toEqual(defaultJesApi);
+    });
+
+    it("registers multiple API instances in parallel", async () => {
+        const mockRefresh = jest.fn(async (): Promise<void> => {return;});
+        Object.defineProperty(Profiles, "getInstance", {
+            value: jest.fn(() => {
+                return {
+                    refresh: mockRefresh
+                };
+            })
+        });
+
+        const api1 = new MockUssApi1();
+        const api2 = new MockUssApi2();
+
+        registry.registerUssApi(api1);
+        registry.reloadProfiles();
+        registry.registerUssApi(api2);
+        await registry.reloadProfiles();
+
+        expect(mockRefresh.mock.calls.length).toBe(2);
+    });
+
+    it("throws errors when registering invalid APIs", () => {
+        const api1 = new MockUssApi1();
+        const mockGetProfileTypeName = jest.fn(() => undefined);
+        api1.getProfileTypeName = mockGetProfileTypeName;
+        expect(() => {registry.registerUssApi(api1);}).toThrow();
+        expect(() => {registry.registerUssApi(undefined);}).toThrow();
+
+        const mvsApi = new ZosmfMvsApi();
+        mvsApi.getProfileTypeName = mockGetProfileTypeName;
+        expect(() => {registry.registerMvsApi(mvsApi);}).toThrow();
+        expect(() => {registry.registerMvsApi(undefined);}).toThrow();
+
+        const jesApi = new ZosmfJesApi();
+        jesApi.getProfileTypeName = mockGetProfileTypeName;
+        expect(() => {registry.registerJesApi(jesApi);}).toThrow();
+        expect(() => {registry.registerJesApi(undefined);}).toThrow();
+    });
+
+    it("throws errors when invalid APIs requested", () => {
+        expect(() => {registry.getUssApi(undefined);}).toThrow();
+        expect(() => {registry.getMvsApi(undefined);}).toThrow();
+        expect(() => {registry.getJesApi(undefined);}).toThrow();
+    });
+});
