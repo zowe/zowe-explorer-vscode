df0a8d4176961e0e9daa84d7638c82a85aebe377
Dan Kelosky <dkelosky@gmail.com>
2020-06-10 15:15:27 -0400

    check for node existence
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

--- src/dataset/actions.ts
+++ src/dataset/actions.ts
@@ -980,7 +980,7 @@ export async function saveFile(doc: vscode.TextDocument, datasetProvider: IZoweT
             location: vscode.ProgressLocation.Notification,
             title: localize("saveFile.response.save.title", "Saving data set...")
         }, () => {
-            const prof = node.getProfile() || profile;
+            const prof = (node) ? node.getProfile() : profile;
             if (prof.profile.encoding) {
                 uploadOptions.encoding = prof.profile.encoding;
             }
@@ -999,7 +999,7 @@ export async function saveFile(doc: vscode.TextDocument, datasetProvider: IZoweT
             } else {
                 const oldDoc = doc;
                 const oldDocText = oldDoc.getText();
-                const prof = node.getProfile() || profile;
+                const prof = (node) ? node.getProfile() : profile;
                 if (prof.profile.encoding) {
                     uploadOptions.encoding = prof.profile.encoding;
                 }
