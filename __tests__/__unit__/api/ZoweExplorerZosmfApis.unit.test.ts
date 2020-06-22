bea5b92c5cc532880f0cadd968d68549af055f91
Dan Kelosky <dkelosky@gmail.com>
2020-06-22 15:24:19 -0400

    unit test zosmf api method
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

---
+++ __tests__/__unit__/api/ZoweExplorerZosmfApis.unit.test.ts
@@ -0,0 +1,43 @@
+/*
+* This program and the accompanying materials are made available under the terms of the *
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at *
+* https://www.eclipse.org/legal/epl-v20.html                                      *
+*                                                                                 *
+* SPDX-License-Identifier: EPL-2.0                                                *
+*                                                                                 *
+* Copyright Contributors to the Zowe Project.                                     *
+*                                                                                 *
+*/
+
+import { ZosmfUssApi } from "../../../src/api/ZoweExplorerZosmfApi";
+import { ZoweExplorerApi } from "../../../src/api/ZoweExplorerApi";
+import { IZosFilesResponse } from "@zowe/cli";
+
+describe("Zosmf API tests", () => {
+
+    it("should test that common putContent is called by putContents", async () => {
+
+        const api = new ZosmfUssApi();
+
+        (api.putContent as any) = jest.fn<ReturnType<typeof api.putContents>, Parameters<typeof api.putContents>>(
+            async (inputFilePath: string, ussFilePath: string,
+                binary?: boolean, localEncoding?: string,
+                etag?: string, returnEtag?: boolean) => {
+
+                return {
+                    success: true,
+                    commandResponse: "whatever"
+                };
+
+            });
+
+        await api.putContents("someLocalFile.txt", "/some/remote", true);
+
+        expect(api.putContent).toBeCalledTimes(1);
+        expect(api.putContent).toBeCalledWith("someLocalFile.txt", "/some/remote", {
+            binary: true,
+        });
+
+    });
+
+});
