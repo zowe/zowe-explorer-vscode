ae11b00bf385d632d89a182f4dddcf2d68cb9e8c
Dan Kelosky <dkelosky@gmail.com>
2020-06-25 14:54:05 -0400

    add test debugging
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

--- __tests__/__theia__/extension.theia.test.ts
+++ __tests__/__theia__/extension.theia.test.ts
@@ -30,7 +30,7 @@ describe("Extension Theia Tests", () => {
 
     it("should open Zowe Explorer and find the Favorites node", async () => {
         await driver.get("http://localhost:3000");
-        const source = (await driver).getPageSource();
+        const source = await driver.getPageSource();
         process.stdout.write(`@source is ${source}`);
         await driver.sleep(SLEEPTIME);
         const button = driver.wait(until.elementLocated(By.id("shell-tab-plugin-view-container:zowe")));
