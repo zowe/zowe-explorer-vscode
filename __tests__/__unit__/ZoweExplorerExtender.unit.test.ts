38463ac2d05814de04e145c208c73e3268add775
Lauren Li <45975633+lauren-li@users.noreply.github.com>
2020-07-29 14:00:07 -0400

    Refinements, add USS tree portion of reloadProfiles test
    Signed-off-by: Lauren Li <45975633+lauren-li@users.noreply.github.com>

--- __tests__/__unit__/ZoweExplorerExtender.unit.test.ts
+++ __tests__/__unit__/ZoweExplorerExtender.unit.test.ts
@@ -8,7 +8,8 @@
 * Copyright Contributors to the Zowe Project.                                     *
 *                                                                                 *
 */
-
+​
+import * as vscode from "vscode";
 import { Profiles } from "../../src/Profiles";
 import { Logger } from "@zowe/imperative";
 import { ZoweExplorerExtender } from "../../src/ZoweExplorerExtender";
@@ -16,63 +17,65 @@ import { createISession, createAltTypeIProfile, createTreeView, createIProfile }
 import { createDatasetSessionNode, createDatasetTree } from "../../__mocks__/mockCreators/datasets";
 import { createUSSSessionNode, createUSSTree } from "../../__mocks__/mockCreators/uss";
 import { createJobsTree, createIJobObject } from "../../__mocks__/mockCreators/jobs";
-
-describe("ZoweExplorerExtender unit tests", async () => {
-    function createBlockMocks() {
-        const session = createISession();
-        const imperativeProfile = createIProfile();
-        const altTypeProfile = createAltTypeIProfile();
-        const treeView = createTreeView();
-        return { session, imperativeProfile, altTypeProfile, treeView };
+​
+describe("ZoweExplorerExtender unit tests", () => {
+    async function createBlockMocks() {
+        const newMocks = {
+            log: Logger.getAppLogger(),
+            session: createISession(),
+            imperativeProfile: createIProfile(),
+            altTypeProfile: createAltTypeIProfile(),
+            treeView: createTreeView(),
+            instTest: ZoweExplorerExtender.getInstance(),
+            profiles: null,
+        };
+​
+        newMocks.profiles = await Profiles.createInstance(newMocks.log);
+        Object.defineProperty(vscode.window, "createTreeView", { value: jest.fn(), configurable: true });
+​
+        return newMocks;
     }
-
-    const log = Logger.getAppLogger();
-    let profiles: Profiles;
-    beforeEach(async () => {
+​
+    afterEach(async () => {
         jest.resetAllMocks();
-        profiles = await Profiles.createInstance(log);
     });
-
+​
     it("calls DatasetTree addSession when extender profiles are reloaded", async () => {
-        const blockMocks = createBlockMocks();
+        const blockMocks = await createBlockMocks();
         const datasetSessionNode = createDatasetSessionNode(blockMocks.session, blockMocks.altTypeProfile);
         const datasetTree = createDatasetTree(datasetSessionNode, blockMocks.altTypeProfile);
         ZoweExplorerExtender.createInstance(datasetTree, undefined, undefined);
-        const instTest  = ZoweExplorerExtender.getInstance();
-        jest.spyOn(instTest.datasetProvider, "addSession");
-        await instTest.reloadProfiles();
-        expect(instTest.datasetProvider.addSession).toHaveBeenCalled();
+        jest.spyOn(blockMocks.instTest.datasetProvider, "addSession");
+        await blockMocks.instTest.reloadProfiles();
+        expect(blockMocks.instTest.datasetProvider.addSession).toHaveBeenCalled();
     });
-
-    // Working on this unit test, but currently running into some mocking issues
-    // it("calls USSTree addSession when extender profiles are reloaded", async () => {
-    //     const blockMocks = createBlockMocks();
-    //     const ussSessionNode = createUSSSessionNode(blockMocks.session, blockMocks.imperativeProfile);
-    //     const ussTree = createUSSTree([], [ussSessionNode], blockMocks.treeView);
-    //     ZoweExplorerExtender.createInstance(undefined, ussTree, undefined);
-    //     const instTest  = ZoweExplorerExtender.getInstance();
-    //     jest.spyOn(instTest.ussFileProvider, "addSession");
-    //     await instTest.reloadProfiles();
-    //     expect(instTest.ussFileProvider.addSession).toHaveBeenCalled();
-    // });
-
+​
+    it("calls USSTree addSession when extender profiles are reloaded", async () => {
+        const blockMocks = await createBlockMocks();
+        const ussSessionNode = createUSSSessionNode(blockMocks.session, blockMocks.imperativeProfile);
+        const ussTree = createUSSTree([], [ussSessionNode], blockMocks.treeView);
+        ZoweExplorerExtender.createInstance(undefined, ussTree, undefined);
+        jest.spyOn(blockMocks.instTest.ussFileProvider, "addSession");
+        await blockMocks.instTest.reloadProfiles();
+        expect(blockMocks.instTest.ussFileProvider.addSession).toHaveBeenCalled();
+    });
+​
     it("calls ZosJobsProvider addSession when extender profiles are reloaded", async () => {
-        const blockMocks = createBlockMocks();
+        const blockMocks = await createBlockMocks();
         const testJob = createIJobObject();
         const jobsTree = createJobsTree(blockMocks.session, testJob, blockMocks.altTypeProfile, blockMocks.treeView);
         ZoweExplorerExtender.createInstance(undefined, undefined, jobsTree);
-        const instTest  = ZoweExplorerExtender.getInstance();
-        jest.spyOn(instTest.jobsProvider, "addSession");
-        await instTest.reloadProfiles();
-        expect(instTest.jobsProvider.addSession).toHaveBeenCalled();
+        jest.spyOn(blockMocks.instTest.jobsProvider, "addSession");
+        await blockMocks.instTest.reloadProfiles();
+        expect(blockMocks.instTest.jobsProvider.addSession).toHaveBeenCalled();
     });
-
+​
     it("does not use any tree providers when the created instance does not provide them", async () => {
+        const blockMocks = await createBlockMocks();
         ZoweExplorerExtender.createInstance();
-        const instTest  = ZoweExplorerExtender.getInstance();
-        await instTest.reloadProfiles();
-        expect(instTest.datasetProvider).toBe(undefined);
-        expect(instTest.ussFileProvider).toBe(undefined);
-        expect(instTest.jobsProvider).toBe(undefined);
+        await blockMocks.instTest.reloadProfiles();
+        expect(blockMocks.instTest.datasetProvider).toBe(undefined);
+        expect(blockMocks.instTest.ussFileProvider).toBe(undefined);
+        expect(blockMocks.instTest.jobsProvider).toBe(undefined);
     });
 });
