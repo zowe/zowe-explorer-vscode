d7f035fc03c64914e32047c037a9780eb9c06e1e
Vit Gottwald <vit.gottwald@gmail.com>
2020-08-11 18:54:52 +0200

    Simplified webpack config
    Signed-off-by: Vit Gottwald <vit.gottwald@gmail.com>

--- packages/zowe-explorer/webpack.config.js
+++ packages/zowe-explorer/webpack.config.js
@@ -20,7 +20,6 @@ var fs = require("fs");
 /**@type {import('webpack').Configuration}*/
 const config = {
     target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-	// mode:'development',
     entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
     output: { // the bundle is stored in the 'out/src' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
         path: path.resolve(__dirname, 'out/src'),
@@ -51,33 +50,24 @@ const config = {
         rules: [{
             test: /\.ts$/,
             exclude: /node_modules/,
-            use: [
-                {
-                    // configure TypeScript loader:
-                    // * enable sources maps for end-to-end source maps
-                    loader: 'ts-loader',
-                    options: {
-                        compilerOptions: {
-                            "sourceMap": true,
-                        }
+            use: [{
+                loader: 'vscode-nls-dev/lib/webpack-loader',
+                options: {
+                    base: __dirname
+                }
+            }, {
+                loader: 'ts-loader',
+                options: {
+                    compilerOptions: {
+                        "sourceMap": true,
                     }
-                },
-        ]},
-    ]},
+                }
+            }]
+        }]
+    },
     plugins: [
         new webpack.BannerPlugin(fs.readFileSync('../../.vscode/banner.txt', 'utf8'))
     ]
-}
-
-// Generates source code folder hierarchy nls metadata files in the output folder (e.g. out/src)
-if (process.argv.includes('--vscode-nls')) {
-	// rewrite nls call when being asked for
-	config.module.rules.unshift({
-		loader: 'vscode-nls-dev/lib/webpack-loader',
-		options: {
-			base: __dirname
-		}
-	})
 }
 
 module.exports = config;
