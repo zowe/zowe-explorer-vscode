b5d5f126ea06a90b975e5a2fa3146d7ebaa2aa27
Katelyn Nienaber <katelyn.nienaber@broadcom.com>
2020-10-12 19:31:29 +0200

    experiments
    Signed-off-by: Katelyn Nienaber <katelyn.nienaber@broadcom.com>

--- __tests__/__theia__/extension.theia.test.ts
+++ __tests__/__theia__/extension.theia.test.ts
@@ -33,7 +33,7 @@ describe("Extension Theia Tests", () => {
         await driver.sleep(SLEEPTIME);
         const button = driver.wait(until.elementLocated(By.id("shell-tab-plugin-view-container:zowe")));
         button.click();
-        const favoriteLink = await driver.findElements((element) => element.class && element.class.includes("theia-TreeNodeSegment"));
+        const favoriteLink = await driver.findElements((element) => element.class && element.class.includes("TreeNodeSegment"));
         let favoriteLink2;
         if (button) {
             favoriteLink2 = driver.findElement(By.xpath("/html"));
@@ -58,7 +58,7 @@ describe("Extension Theia Tests", () => {
                 By.xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div[2]/div[8]/div/div[1]/div[2]/div[1]/div/div[1]/div/div/div[1]"));
         }
         // const favoriteLink = await driver.wait(until.elementLocated(By.id("/0:Favorites")), WAITTIME).getAttribute("title");
-        expect(favoriteLink).to.equal("Favorites");
+        expect(favoriteLink[0].getAttribute("title")).to.equal("Favorites");
         expect(favoriteLink2.getAttribute("title")).to.be("Favorites");
     }).timeout(TIMEOUT);
 
