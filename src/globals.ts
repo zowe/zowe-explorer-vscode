e2eb33f18cfdabd9544ce1e81d0b58cbddbe6028
Katelyn Nienaber <katelyn.nienaber@broadcom.com>
2020-10-13 16:55:34 +0200

    ran prepublish
    Signed-off-by: Katelyn Nienaber <katelyn.nienaber@broadcom.com>

--- src/globals.ts
+++ src/globals.ts
@@ -21,7 +21,7 @@ export let USS_DIR;
 export let DS_DIR;
 export let ISTHEIA: boolean = false; // set during activate
 export let LOG: Logger;
-export const COMMAND_COUNT = 84;
+export const COMMAND_COUNT = 85;
 export const CONTEXT_PREFIX = "_";
 export const FAV_SUFFIX = CONTEXT_PREFIX + "fav";
 export const FAV_PROFILE_CONTEXT = "profile_fav";
