c072bd3f95de9891eb48edd8fad7e2cd304287c2
zFernand0 <fernando.rijocedeno@broadcom.com>
2020-11-10 15:26:17 -0500

    add npm publishing
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- .github/actions/deploy-npm-package/index.js
+++ .github/actions/deploy-npm-package/index.js
@@ -21,16 +21,16 @@ try {
   const packageJson = JSON.parse(readFileSync(path.join(packagePath, "package.json")));
 
   // Gather the extension information
-  let extensionMetadata = null;
+  let packageMetadata = null;
   try {
-    extensionMetadata = JSON.parse(execSync(`vsce show ${packageJson.publisher}.${packageJson.name} --json`).toString());
+    packageMetadata = JSON.parse(execSync(`npm view ${packageJson.name} --json`).toString());
   } catch (err) {
     // Do nothing if the package was not found and just continue to publish the extension
     console.log(`Package ${packageJson.publisher}.${packageJson.name} not found!`);
   }
 
   // Check if there is a new version to publish (looking at the top level package.json for version)
-  if (extensionMetadata != null && extensionMetadata.versions[0].version == topPackageJson.version) {
+  if (packageMetadata != null && packageMetadata.versions.includes(topPackageJson.version)) {
     console.log(`No new version to publish at this time. Current version: ${topPackageJson.version}`);
   } else {
     console.log(`Incrementing version: ${packageJson.version} -> ${topPackageJson.version}`);
@@ -39,13 +39,16 @@ try {
     console.log(execSync(`git status`, {cwd: packagePath}).toString());
     console.log(execSync(`git add package.json`, {cwd: packagePath}).toString());
 
-    console.log(`Publishing version ${topPackageJson.version}`);
-    console.log(execSync(`vsce publish --yarn -p ${core.getInput("token")}`, {cwd: packagePath}).toString());
-
     const versionName = `${packageJson.name}-${topPackageJson.version}`;
     console.log(`Generate: ${versionName}`);
     console.log(execSync(`yarn package`, {cwd: packagePath}).toString());
     core.setOutput("archive", versionName);
+
+    console.log(execSync(`echo registry=https://registry.npmjs.org/ > .npmrc`, {cwd: packagePath}).toString());
+    console.log(execSync(`//registry.npmjs.org/:_authToken=${core.getInput("token")} >> .npmrc`, {cwd: packagePath}).toString());
+
+    console.log(`Publishing version ${topPackageJson.version}`);
+    console.log(execSync(`npm publish --access public`, {cwd: packagePath}).toString());
 
     let changelog = execSync("awk -v ver=" + topPackageJson.version + " '/## / {if (p) { exit }; if (\$2 ~ ver) { p=1; next} } p && NF' CHANGELOG.md | sed -z \"s/'/'\\\\\\''/g\" | sed -z 's/\"/\\\"/g' | sed -z 's/\\n/\\\\n/g'", {cwd: packagePath}).toString();
     if (changelog != "") {
