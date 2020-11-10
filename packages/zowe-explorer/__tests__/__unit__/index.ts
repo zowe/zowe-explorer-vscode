393914a005610c8ee89f07050a81cae6c235d7fe
Lauren Li <45975633+lauren-li@users.noreply.github.com>
2020-11-10 18:13:10 -0500

    First pass of working testrunner migration
    Signed-off-by: Lauren Li <45975633+lauren-li@users.noreply.github.com>

--- packages/zowe-explorer/__tests__/__unit__/index.ts
+++ packages/zowe-explorer/__tests__/__unit__/index.ts
@@ -21,14 +21,41 @@
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
 
-// tslint:disable-next-line:no-submodule-imports
-import * as testRunner from "vscode/lib/testrunner";
+import * as path from "path";
+import * as Mocha from "mocha";
+import * as glob from "glob";
 
-// You can directly control Mocha options by uncommenting the following lines
-// See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
-testRunner.configure({
-    ui: "tdd", // the TDD UI is being used in extension.test.ts (suite, test, etc.)
-    useColors: true, // colored output from test results
-});
+export function run(): Promise<void> {
+    // Create the mocha test
+    const mocha = new Mocha({
+        ui: "bdd",
+    });
+    mocha.useColors(true);
 
-module.exports = testRunner;
+    const testsRoot = __dirname;
+
+    return new Promise((c, e) => {
+        glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
+            if (err) {
+                return e(err);
+            }
+
+            // Add files to the test suite
+            files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+
+            try {
+                // Run the mocha test
+                mocha.run((failures) => {
+                    if (failures > 0) {
+                        e(new Error(`${failures} tests failed.`));
+                    } else {
+                        c();
+                    }
+                });
+            } catch (err) {
+                console.error(err);
+                e(err);
+            }
+        });
+    });
+}
