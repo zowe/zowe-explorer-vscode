5f21c5b7d6e66a25bad7b70c145e35587b8536ca
Katelyn Nienaber <katelyn.nienaber@broadcom.com>
2021-08-10 10:33:06 +0200

    WIP Imperative logger for extenders
    Signed-off-by: Katelyn Nienaber <katelyn.nienaber@broadcom.com>
    Signed-off-by: Anokhi Kastia <90722760+anokhikastiaIBM@users.noreply.github.com>

--- packages/zowe-explorer-ftp-extension/src/extension.ts
+++ packages/zowe-explorer-ftp-extension/src/extension.ts
@@ -10,7 +10,7 @@
  */
 
 import * as vscode from "vscode";
-import { ZoweExplorerApi, ZoweVsCodeExtension } from "@zowe/zowe-explorer-api";
+import { ZoweExplorerApi, ZoweVsCodeExtension, IZoweLogger } from "@zowe/zowe-explorer-api";
 import { FtpUssApi } from "./ZoweExplorerFtpUssApi";
 import { FtpMvsApi } from "./ZoweExplorerFtpMvsApi";
 import { FtpJesApi } from "./ZoweExplorerFtpJesApi";
@@ -19,6 +19,7 @@ import { CoreUtils } from "@zowe/zos-ftp-for-zowe-cli";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function activate(context: vscode.ExtensionContext): void {
     void registerFtpApis();
+    const imperativeLogger = new IZoweLogger();
 }
 
 /**
