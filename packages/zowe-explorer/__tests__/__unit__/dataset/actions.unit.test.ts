32da4788dd11da05d52010032c67763f9d7a5040
Anokhi Kastia <90722760+anokhikastiaIBM@users.noreply.github.com>
2021-09-23 10:55:35 -0400

    Signed off commit

--- packages/zowe-explorer/__tests__/__unit__/dataset/actions.unit.test.ts
+++ packages/zowe-explorer/__tests__/__unit__/dataset/actions.unit.test.ts
@@ -61,6 +61,7 @@ function createGlobalMocks() {
         testFavoritesNode: createDatasetFavoritesNode(),
         testDatasetTree: null,
         mvsApi: null,
+        mockShowWarningMessage: jest.fn(),
     };
 
     newMocks.profileInstance = createInstanceOfProfile(newMocks.imperativeProfile);
@@ -544,6 +545,7 @@ describe("Dataset Actions Unit Tests - Function deleteDatasetPrompt", () => {
         const selectedNodes = [blockMocks.testDatasetNode];
         const treeView = createTreeView(selectedNodes);
         blockMocks.testDatasetTree.getTreeView.mockReturnValueOnce(treeView);
+        globalMocks.mockShowWarningMessage.mockResolvedValueOnce("Delete");
 
         await dsActions.deleteDatasetPrompt(blockMocks.testDatasetTree);
 
@@ -559,6 +561,7 @@ describe("Dataset Actions Unit Tests - Function deleteDatasetPrompt", () => {
         const selectedNodes = [blockMocks.testMemberNode];
         const treeView = createTreeView(selectedNodes);
         blockMocks.testDatasetTree.getTreeView.mockReturnValueOnce(treeView);
+        globalMocks.mockShowWarningMessage.mockResolvedValueOnce("Delete");
 
         await dsActions.deleteDatasetPrompt(blockMocks.testDatasetTree);
 
@@ -576,6 +579,7 @@ describe("Dataset Actions Unit Tests - Function deleteDatasetPrompt", () => {
         const selectedNodes = [blockMocks.testVsamNode];
         const treeView = createTreeView(selectedNodes);
         blockMocks.testDatasetTree.getTreeView.mockReturnValueOnce(treeView);
+        globalMocks.mockShowWarningMessage.mockResolvedValueOnce("Delete");
 
         await dsActions.deleteDatasetPrompt(blockMocks.testDatasetTree);
 
@@ -591,6 +595,7 @@ describe("Dataset Actions Unit Tests - Function deleteDatasetPrompt", () => {
         const selectedNodes = [blockMocks.testDatasetNode, blockMocks.testVsamNode];
         const treeView = createTreeView(selectedNodes);
         blockMocks.testDatasetTree.getTreeView.mockReturnValueOnce(treeView);
+        globalMocks.mockShowWarningMessage.mockResolvedValueOnce("Delete");
 
         await dsActions.deleteDatasetPrompt(blockMocks.testDatasetTree);
 
@@ -599,18 +604,19 @@ describe("Dataset Actions Unit Tests - Function deleteDatasetPrompt", () => {
         );
     });
 
-    it("Should delete one dataset and one member", async () => {
+    it("Should delete only member since parent is selected data set", async () => {
         const globalMocks = createGlobalMocks();
         const blockMocks = createBlockMocks(globalMocks);
 
         const selectedNodes = [blockMocks.testMemberNode, blockMocks.testDatasetNode];
         const treeView = createTreeView(selectedNodes);
         blockMocks.testDatasetTree.getTreeView.mockReturnValueOnce(treeView);
+        globalMocks.mockShowWarningMessage.mockResolvedValueOnce("Delete");
 
         await dsActions.deleteDatasetPrompt(blockMocks.testDatasetTree);
 
         expect(mocked(vscode.window.showInformationMessage)).toBeCalledWith(
-            `The following items were deleted: ${blockMocks.testDatasetNode.getLabel()}(${blockMocks.testMemberNode.getLabel()}), ${blockMocks.testDatasetNode.getLabel()}`
+            `The following items were deleted: ${blockMocks.testDatasetNode.getLabel()}(${blockMocks.testMemberNode.getLabel()})`
         );
     });
 
@@ -621,6 +627,7 @@ describe("Dataset Actions Unit Tests - Function deleteDatasetPrompt", () => {
         const selectedNodes = [blockMocks.testFavoritedNode];
         const treeView = createTreeView(selectedNodes);
         blockMocks.testDatasetTree.getTreeView.mockReturnValueOnce(treeView);
+        globalMocks.mockShowWarningMessage.mockResolvedValueOnce("Cancel");
 
         await dsActions.deleteDatasetPrompt(blockMocks.testDatasetTree);
 
@@ -634,6 +641,7 @@ describe("Dataset Actions Unit Tests - Function deleteDatasetPrompt", () => {
         const selectedNodes = [blockMocks.testFavMemberNode];
         const treeView = createTreeView(selectedNodes);
         blockMocks.testDatasetTree.getTreeView.mockReturnValueOnce(treeView);
+        globalMocks.mockShowWarningMessage.mockResolvedValueOnce("Cancel");
 
         await dsActions.deleteDatasetPrompt(blockMocks.testDatasetTree);
 
@@ -647,6 +655,7 @@ describe("Dataset Actions Unit Tests - Function deleteDatasetPrompt", () => {
         const selectedNodes = [globalMocks.datasetSessionNode];
         const treeView = createTreeView(selectedNodes);
         blockMocks.testDatasetTree.getTreeView.mockReturnValueOnce(treeView);
+        globalMocks.mockShowWarningMessage.mockResolvedValueOnce("Cancel");
 
         await dsActions.deleteDatasetPrompt(blockMocks.testDatasetTree);
 
@@ -660,6 +669,7 @@ describe("Dataset Actions Unit Tests - Function deleteDatasetPrompt", () => {
         const selectedNodes = [blockMocks.testFavoritedNode, blockMocks.testDatasetNode];
         const treeView = createTreeView(selectedNodes);
         blockMocks.testDatasetTree.getTreeView.mockReturnValueOnce(treeView);
+        globalMocks.mockShowWarningMessage.mockResolvedValueOnce("Delete");
 
         await dsActions.deleteDatasetPrompt(blockMocks.testDatasetTree);
 
@@ -672,7 +682,7 @@ describe("Dataset Actions Unit Tests - Function deleteDatasetPrompt", () => {
         const globalMocks = createGlobalMocks();
         const blockMocks = createBlockMocks(globalMocks);
 
-        mocked(vscode.window.showQuickPick).mockResolvedValueOnce("Cancel" as any);
+        globalMocks.mockShowWarningMessage.mockResolvedValueOnce("Cancel");
 
         await dsActions.deleteDatasetPrompt(blockMocks.testDatasetTree);
 
