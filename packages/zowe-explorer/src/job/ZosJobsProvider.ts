a7274e124bc521708aab617ad4a7ec466492a15a
Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
2021-09-27 12:27:30 -0400

    remove double confirmation, favProfile removal VSC dialog
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>

--- packages/zowe-explorer/src/job/ZosJobsProvider.ts
+++ packages/zowe-explorer/src/job/ZosJobsProvider.ts
@@ -368,7 +368,7 @@ export class ZosJobsProvider extends ZoweTreeProvider implements IZoweTree<IZowe
                 const btnLabelRemove = localize("initializeJobsFavorites.error.buttonRemove", "Remove");
                 vscode.window.showErrorMessage(errMessage, { modal: true }, btnLabelRemove).then(async (selection) => {
                     if (selection === btnLabelRemove) {
-                        await this.removeFavProfile(profileName, true);
+                        await this.removeFavProfile(profileName, false);
                     }
                 });
                 return;
@@ -503,6 +503,7 @@ export class ZosJobsProvider extends ZoweTreeProvider implements IZoweTree<IZowe
      */
     public async removeFavProfile(profileName: string, userSelected: boolean) {
         // If user selected the "Remove profile from Favorites option", confirm they are okay with deleting all favorited items for that profile.
+        let cancelled = false;
         if (userSelected) {
             const checkConfirmation = localize(
                 "removeFavProfile.confirm",
@@ -510,18 +511,16 @@ export class ZosJobsProvider extends ZoweTreeProvider implements IZoweTree<IZowe
                 profileName
             );
             const continueRemove = localize("removeFavProfile.continue", "Continue");
-            const cancelRemove = localize("removeFavProfile.cancel", "Cancel");
-            const quickPickOptions: vscode.QuickPickOptions = {
-                placeHolder: checkConfirmation,
-                ignoreFocusOut: true,
-                canPickMany: false,
-            };
-            // If user did not select "Continue", do nothing.
-            if (
-                (await vscode.window.showQuickPick([continueRemove, cancelRemove], quickPickOptions)) !== continueRemove
-            ) {
-                return;
-            }
+            await vscode.window
+                .showWarningMessage(checkConfirmation, { modal: true }, ...[continueRemove])
+                .then((selection) => {
+                    if (!selection || selection === "Cancel") {
+                        cancelled = true;
+                    }
+                });
+        }
+        if (cancelled) {
+            return;
         }
 
         // Remove favorited profile from UI
--- packages/zowe-explorer/src/uss/USSTree.ts
+++ packages/zowe-explorer/src/uss/USSTree.ts
@@ -467,6 +467,7 @@ export class USSTree extends ZoweTreeProvider implements IZoweTree<IZoweUSSTreeN
      */
     public async removeFavProfile(profileName: string, userSelected: boolean) {
         // If user selected the "Remove profile from Favorites option", confirm they are okay with deleting all favorited items for that profile.
+        let cancelled = false;
         if (userSelected) {
             const checkConfirmation = localize(
                 "removeFavProfile.confirm",
@@ -474,18 +475,16 @@ export class USSTree extends ZoweTreeProvider implements IZoweTree<IZoweUSSTreeN
                 profileName
             );
             const continueRemove = localize("removeFavProfile.continue", "Continue");
-            const cancelRemove = localize("removeFavProfile.cancel", "Cancel");
-            const quickPickOptions: vscode.QuickPickOptions = {
-                placeHolder: checkConfirmation,
-                ignoreFocusOut: true,
-                canPickMany: false,
-            };
-            // If user did not select "Continue", do nothing.
-            if (
-                (await vscode.window.showQuickPick([continueRemove, cancelRemove], quickPickOptions)) !== continueRemove
-            ) {
-                return;
-            }
+            await vscode.window
+                .showWarningMessage(checkConfirmation, { modal: true }, ...[continueRemove])
+                .then((selection) => {
+                    if (!selection || selection === "Cancel") {
+                        cancelled = true;
+                    }
+                });
+        }
+        if (cancelled) {
+            return;
         }
 
         // Remove favorited profile from UI
@@ -815,7 +814,7 @@ export class USSTree extends ZoweTreeProvider implements IZoweTree<IZoweUSSTreeN
                 const btnLabelRemove = localize("initializeUSSFavorites.error.buttonRemove", "Remove");
                 vscode.window.showErrorMessage(errMessage, { modal: true }, btnLabelRemove).then(async (selection) => {
                     if (selection === btnLabelRemove) {
-                        await this.removeFavProfile(profileName, true);
+                        await this.removeFavProfile(profileName, false);
                     }
                 });
                 return;
