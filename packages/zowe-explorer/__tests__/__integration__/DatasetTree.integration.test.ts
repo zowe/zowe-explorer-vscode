26e7c33e192382634dea5aaa656e4c3a006dd18b
Stanko Uzunov <su670571@broadcom.net>
2021-12-08 08:38:45 +0100

    Remove safe-buffer
    Signed-off-by: Stanko Uzunov <su670571@broadcom.net>

--- packages/zowe-explorer/__tests__/__integration__/DatasetTree.integration.test.ts
+++ packages/zowe-explorer/__tests__/__integration__/DatasetTree.integration.test.ts
@@ -21,7 +21,6 @@ import * as sinon from "sinon";
 import * as chai from "chai";
 import * as chaiAsPromised from "chai-as-promised";
 import * as globals from "../../src/globals";
-const Buffer = require("safer-buffer").Buffer;
 
 declare var it: any;
 
