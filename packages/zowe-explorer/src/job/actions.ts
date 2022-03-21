f2d56754f368294f65e83a99c61d0b6daf6acac2
Rudy Leonel Pichola Flores <rudyflores@ibm.com>
2022-03-21 10:32:27 -0400

    Fix broken test

--- packages/zowe-explorer/src/job/actions.ts
+++ packages/zowe-explorer/src/job/actions.ts
@@ -78,8 +78,8 @@ export async function getSpoolContent(session: string, spool: zowe.IJobFile, ref
             await vscode.window.showTextDocument(uri);
         } catch (error) {
             const isTextDocActive =
-                vscode.window.activeTextEditor.document.uri.path === `${spool.jobname}.${spool.jobid}.${spool.ddname}`;
-            if (error.message.toString().contains("Failed to show text document") && isTextDocActive) {
+                vscode.window.activeTextEditor.document.uri?.path === `${spool.jobname}.${spool.jobid}.${spool.ddname}`;
+            if (String(error.message).includes("Failed to show text document") && isTextDocActive) {
                 return;
             }
             await errorHandling(error, session, error.message);
