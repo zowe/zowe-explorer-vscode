f4d1a60bc6f7caa73dba21ab0039f62d4b5d6140
Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
2022-03-28 10:57:54 -0400

    try try again
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>

--- packages/zowe-explorer/__tests__/__theia__/theia/extension.theiaChrome.ts
+++ packages/zowe-explorer/__tests__/__theia__/theia/extension.theiaChrome.ts
@@ -69,7 +69,7 @@ export async function clickOnJobsTab() {
 
 export async function getFavoritePrfileNameFromDatasets() {
     const favoriteProfile = await driverChrome
-        .wait(until.elementLocated(By.xpath(DatasetsLocators.favoriteProfileInDatasetXpath)), WAITTIME)
+        .wait(until.elementLocated(By.id(DatasetsLocators.favoriteProfileInDatasetId)), WAITTIME)
         .getText();
     return favoriteProfile;
 }
@@ -89,7 +89,7 @@ export async function getFavoritePrfileNameFromJobs() {
 
 export async function removeFavoriteProfileFromDatasets() {
     const removeFromFavorite = await driverChrome.wait(
-        until.elementLocated(By.id(DatasetsLocators.favoriteProfileInDatasetId)),
+        until.elementLocated(By.xpath(DatasetsLocators.favoriteProfileInDatasetXpath)),
         WAITTIME
     );
     await driverChrome.actions().click(removeFromFavorite, Button.RIGHT).perform();
