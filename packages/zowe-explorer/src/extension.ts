cf470026ad3a2d16585c353640f7c40dfcd997df
KutluOzel-b <kutlu.ozel@broadcom.com>
2022-10-18 11:08:30 +0200

    removed unnecessary lines
    Signed-off-by: KutluOzel-b <kutlu.ozel@broadcom.com>

--- packages/zowe-explorer/src/extension.ts
+++ packages/zowe-explorer/src/extension.ts
@@ -8,7 +8,6 @@
  * Copyright Contributors to the Zowe Project.                                     *
  *                                                                                 *
  */
-
 import * as fs from "fs";
 import * as path from "path";
 import * as globals from "./globals";
