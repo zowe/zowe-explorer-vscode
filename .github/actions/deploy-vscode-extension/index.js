a9f84263e111102d5a88d35637d782f841f1acf8
zFernand0 <fernando.rijocedeno@broadcom.com>
2022-10-27 17:10:42 +0000

    [to-be-reverted] skip ZE deployment
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- .github/actions/deploy-vscode-extension/index.js
+++ .github/actions/deploy-vscode-extension/index.js
@@ -33,9 +33,10 @@ const checkVersion = (packageJson, version) => {
 const publishSpecificProject = (versionName, token, packagePath, tag) => {
     if (tag !== "next") {
         console.log(`Publishing: ${versionName}`);
-        if (packagePath !== "packages/zowe-explorer")
-        console.log(execSync(`vsce publish --yarn -p ${token.split(" ")[0]}`, { cwd: packagePath }).toString());
-        console.log(execSync(`ovsx publish ${versionName}.vsix -p ${token.split(" ")[1]}`, { cwd: "dist" }).toString());
+        if (packagePath !== "packages/zowe-explorer") {
+            console.log(execSync(`vsce publish --yarn -p ${token.split(" ")[0]}`, { cwd: packagePath }).toString());
+            console.log(execSync(`ovsx publish ${versionName}.vsix -p ${token.split(" ")[1]}`, { cwd: "dist" }).toString());
+        }
     } else {
         console.log(`Not publishing ${versionName}.vsix with the "next" tag.`);
     }
