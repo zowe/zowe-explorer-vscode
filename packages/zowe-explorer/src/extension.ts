8815aad80a8d792fd2e66fd998f08566b9c51230
Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
2022-11-11 17:35:00 -0500

    GHA test
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>

--- packages/zowe-explorer/src/extension.ts
+++ packages/zowe-explorer/src/extension.ts
@@ -8,7 +8,6 @@
  * Copyright Contributors to the Zowe Project.                                     *
  *                                                                                 *
  */
-
 // kick of theia tests
 
 import * as fs from "fs";
