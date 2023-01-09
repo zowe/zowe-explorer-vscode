566d0564751c6421771cf975d86d46723208bea0
Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
2023-01-09 16:00:03 -0500

    remove success message from when error is returned
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>

--- packages/zowe-explorer/src/job/actions.ts
+++ packages/zowe-explorer/src/job/actions.ts
@@ -326,12 +326,10 @@ async function deleteSingleJob(job: IZoweJobTreeNode, jobsProvider: IZoweTree<IZ
 
     try {
         await jobsProvider.delete(job);
+        vscode.window.showInformationMessage(localize("deleteCommand.job", "Job {0} was deleted.", jobName));
     } catch (error) {
         await errorHandling(error.toString(), job.getProfile().name, error.message.toString());
-        return;
     }
-
-    vscode.window.showInformationMessage(localize("deleteCommand.job", "Job {0} was deleted.", jobName));
 }
 
 async function deleteMultipleJobs(jobs: ReadonlyArray<IZoweJobTreeNode>, jobsProvider: IZoweTree<IZoweJobTreeNode>): Promise<void> {
