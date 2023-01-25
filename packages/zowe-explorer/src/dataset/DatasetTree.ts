728299bf1e7895147750337442d07619ccbdf6de
adam-wolfe <122040687+adam-wolfe@users.noreply.github.com>
2023-01-25 07:57:21 -0500

    Update DatasetTree.ts
    Deleted extra empty line.
    
    Signed-off-by: adam-wolfe <122040687+adam-wolfe@users.noreply.github.com>

--- packages/zowe-explorer/src/dataset/DatasetTree.ts
+++ packages/zowe-explorer/src/dataset/DatasetTree.ts
@@ -873,7 +873,6 @@ export class DatasetTree extends ZoweTreeProvider implements IZoweTree<IZoweData
                 // executing search from saved search in favorites
                 pattern = node.getLabel() as string;
                 const sessionName = node.getProfileName();
-
                 await this.addSession(sessionName);
                 nonFaveNode = this.mSessionNodes.find((tempNode) => tempNode.label.toString() === sessionName);
                 if (!nonFaveNode.getSession().ISession.user || !nonFaveNode.getSession().ISession.password) {
