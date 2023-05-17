77652ded14dd60e6110236496d38061652578944
Timothy Johnson <timothy.johnson@broadcom.com>
2023-05-17 18:11:08 -0400

    Add ESLint rule for no floating promises
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

---
+++ packages/eslint-plugin-zowe-explorer/tests/lib/rules/no-floating-promises.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const rule = require("../../../lib/rules/no-floating-promises");
+const { ESLintUtils } = require("@typescript-eslint/utils");
+
+const ruleTester = new ESLintUtils.RuleTester({
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2015,
+    project: "./tsconfig.eslint.json",
+    sourceType: "module",
+    tsconfigRootDir: __dirname + "/.."
+  }
+});
+
+ruleTester.run("no-floating-promises", rule, {
+  valid: [
+    {
+      code: "await Promise.resolve(true);",
+      options: [{ ignoreVoid: false }],
+    },
+    {
+      code: "import * as vscode from 'vscode';\nvscode.window.showInformationMessage('hello');",
+      options: [{ ignoreVoid: false }],
+    }
+  ],
+
+  invalid: [
+    {
+      code: "Promise.resolve(true);",
+      options: [{ ignoreVoid: false }],
+      errors: [
+        {
+          line: 1,
+          messageId: "floating",
+          suggestions: [
+            {
+              messageId: "floatingFixAwait",
+              output: "await Promise.resolve(true);"
+            }
+          ]
+        }
+      ],
+    }
+  ],
+});
