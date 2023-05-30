f920b70576bede54d4475da2bb9f4a0815770c50
Simon Longpré-Landry <simonllandry@hotmail.com>
2023-05-30 17:40:53 -0400

    Added spool file id to fix #2315 and unit tests
    Signed-off-by: Simon Longpré-Landry <simonllandry@hotmail.com>

--- packages/zowe-explorer/src/SpoolProvider.ts
+++ packages/zowe-explorer/src/SpoolProvider.ts
@@ -85,7 +85,14 @@ export function encodeJobFile(session: string, spool: zowe.IJobFile): vscode.Uri
     ZoweLogger.trace("SpoolProvider.encodeJobFile called.");
     const query = JSON.stringify([session, spool]);
 
-    const spoolSegments = [spool.jobname, spool.jobid, spool.stepname, spool.procstep, spool.ddname];
+    const spoolSegments = [
+        spool.jobname, 
+        spool.jobid, 
+        spool.stepname, 
+        spool.procstep, 
+        spool.ddname, 
+        spool.id?.toString()
+    ];
 
     const path = spoolSegments.filter((v) => v && v.length).join(".");
 
