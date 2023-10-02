ef4c4bba113b85c25062bda65cb2cac989835738
Rudy Flores <68666202+rudyflores@users.noreply.github.com>
2023-10-02 10:12:58 -0400

    update description of webviews creation function
    Signed-off-by: Rudy Flores <68666202+rudyflores@users.noreply.github.com>

--- packages/zowe-explorer-api/src/vscode/ui/WebView.ts
+++ packages/zowe-explorer-api/src/vscode/ui/WebView.ts
@@ -36,10 +36,10 @@ export class WebView {
 
     /**
      * Constructs a webview for use with bundled assets.
-     * The webview entrypoint must be located at <webview folder>/dist/assets/index.js.
+     * The webview entrypoint must be located at src/<webview folder>/dist/<webview-name>/index.js.
      *
      * @param title The title for the new webview
-     * @param webviewName The directory name (relative to the "webviews" folder in the extension root) with a valid entrypoint (see above).
+     * @param webviewName The webview name, the same name given to the directory of your webview in the webviews/src directory.
      * @param context The VSCode extension context
      * @param onDidReceiveMessage Event callback: called when messages are received from the webview
      */
