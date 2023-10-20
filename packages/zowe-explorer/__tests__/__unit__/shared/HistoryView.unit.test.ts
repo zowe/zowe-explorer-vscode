415cb12fda5af8ced9ea5a0c001f8453638c7b90
Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
2023-10-20 09:36:56 -0400

    fix tests and add header to file
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>

--- packages/zowe-explorer/__tests__/__unit__/shared/HistoryView.unit.test.ts
+++ packages/zowe-explorer/__tests__/__unit__/shared/HistoryView.unit.test.ts
@@ -1,3 +1,15 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+import * as vscode from "vscode";
 import { HistoryView } from "../../../src/shared/HistoryView";
 import { createDatasetSessionNode, createDatasetTree } from "../../../__mocks__/mockCreators/datasets";
 import { createUSSSessionNode, createUSSTree } from "../../../__mocks__/mockCreators/uss";
@@ -39,6 +51,7 @@ function createGlobalMocks(): any {
         value: jest.fn().mockReturnValue(createInstanceOfProfile(globalMocks.imperativeProfile)),
         configurable: true,
     });
+    Object.defineProperty(vscode.window, "createTreeView", { value: jest.fn().mockReturnValueOnce(globalMocks.treeView), configurable: true });
 
     return globalMocks;
 }
