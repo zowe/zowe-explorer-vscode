cd4ac6bcf8dde559245304ce48f3368c68cc3518
zFernand0 <37381190+zFernand0@users.noreply.github.com>
2024-03-05 13:50:51 +0000

    update profiles.ts
    Signed-off-by: zFernand0 <37381190+zFernand0@users.noreply.github.com>

--- packages/zowe-explorer/src/Profiles.ts
+++ packages/zowe-explorer/src/Profiles.ts
@@ -885,7 +885,7 @@ export class Profiles extends ProfilesCache {
         }
         const usingSecureCreds = SettingsConfig.getDirectValue(globals.SETTINGS_SECURE_CREDENTIALS_ENABLED);
         const profInfo = await this.getProfileInfo();
-        if (!zowe.imperative.ProfileInfo.onlyV1ProfilesExist && usingSecureCreds) {
+        if (!imperative.ProfileInfo.onlyV1ProfilesExist && usingSecureCreds) {
             return profInfo.getTeamConfig().api.secure.securePropsForProfile(profileName);
         }
         const profAttrs = await this.getProfileFromConfig(profileName);
