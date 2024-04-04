b4c55e982ab6fbe4188fd36ae211ff9f934a8b18
Rudy Flores <68666202+rudyflores@users.noreply.github.com>
2024-04-04 13:08:29 -0400

    fix broken unit test
    Signed-off-by: Rudy Flores <68666202+rudyflores@users.noreply.github.com>

--- packages/zowe-explorer/__tests__/__unit__/utils/ProfilesUtils.unit.test.ts
+++ packages/zowe-explorer/__tests__/__unit__/utils/ProfilesUtils.unit.test.ts
@@ -1009,9 +1009,10 @@ describe("ProfilesUtils unit tests", () => {
 
         it("should prompt whether to disable credential management, and throw error if 'No'", async () => {
             warningMessageSpy.mockResolvedValue("No");
-            await expect(profUtils.ProfilesUtils.promptAndDisableCredentialManagement()).rejects
-                .toThrowError(`Failed to load credential manager. This may be related to Zowe Explorer being unable 
-                    to use the default credential manager in a browser based environment.`);
+            await expect(profUtils.ProfilesUtils.promptAndDisableCredentialManagement()).rejects.toThrowError(
+                // eslint-disable-next-line max-len
+                "Failed to load credential manager. This may be related to Zowe Explorer being unable to use the default credential manager in a browser based environment."
+            );
         });
     });
 });
