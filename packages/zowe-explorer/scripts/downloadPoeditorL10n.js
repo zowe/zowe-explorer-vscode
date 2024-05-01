5e957c80b71803e02a8819e3e65250cd722ef065
Timothy Johnson <timothy.johnson@broadcom.com>
2024-05-01 16:18:06 -0400

    Allow users to contribute translations with POEditor
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

---
+++ packages/zowe-explorer/scripts/downloadPoeditorL10n.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const poeditorToken = process.env.POEDITOR_TOKEN;
+const projectId = 698824;
+const defaultHeaders = { "Content-Type": "application/x-www-form-urlencoded" };
+(async () => {
+    const listResponse = await fetch("https://api.poeditor.com/v2/languages/list", {
+        method: "POST",
+        body: `api_token=${poeditorToken}&id=${projectId}`,
+        headers: defaultHeaders
+    }).then(r => r.json());
+    for (const { code } of listResponse.result.languages) {
+        const exportResponse = await fetch("https://api.poeditor.com/v2/projects/export", {
+            method: "POST",
+            body: `api_token=${poeditorToken}&id=${projectId}&language=${code}&type=key_value_json`,
+            headers: defaultHeaders
+        }).then(r => r.json());
+        const packageNls = {};
+        const l10nBundle = {};
+        for (const [k, v] of Object.entries(await fetch(exportResponse.result.url).then(r => r.json()))) {
+            if (typeof v !== "string") {
+                packageNls[k] = Object.values(v)[0] || undefined;
+            } else {
+                l10nBundle[k] = v || undefined;
+            }
+        }
+        fs.writeFileSync(`${__dirname}/../package.nls.${code}.json`, JSON.stringify(packageNls, null, 2));
+        fs.writeFileSync(`${__dirname}/../l10n/bundle.l10n.${code}.json`, JSON.stringify(l10nBundle, null, 2));
+    }
+})();
---
+++ packages/zowe-explorer/scripts/generatePoeditorJson.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const poeditorJson = {};
+const packageNls = require(__dirname + "/../package.nls.json");
+for (const [k, v] of Object.entries(packageNls)) {
+    poeditorJson[k] = {[v]: ""};
+}
+const l10nBundle = require(__dirname + "/../l10n/bundle.l10n.json");
+for (const [k, v] of Object.entries(l10nBundle)) {
+    poeditorJson[typeof v === "string" ? k : v.message] = "";
+}
+fs.writeFileSync(__dirname + "/../l10n/poeditor.json", JSON.stringify(poeditorJson, null, 2));
