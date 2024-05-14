c1d109e5d19aea31c80280a374445c30e41ef25e
Rudy Flores <68666202+rudyflores@users.noreply.github.com>
2024-05-14 15:15:58 -0400

    Fix broken api tests
    Signed-off-by: Rudy Flores <68666202+rudyflores@users.noreply.github.com>

--- packages/zowe-explorer-api/__tests__/__unit__/fs/utils/datasets.unit.test.ts
+++ packages/zowe-explorer-api/__tests__/__unit__/fs/utils/datasets.unit.test.ts
@@ -9,40 +9,40 @@
  *
  */
 
-import { DsEntry, MemberEntry, PdsEntry, isDsEntry, isMemberEntry, isPdsEntry } from "../../../../src";
+import { DsEntry, MemberEntry, PdsEntry, FsDatasetsUtils } from "../../../../src";
 
 describe("isDsEntry", () => {
     it("returns true if value is a DsEntry", () => {
         const entry = new DsEntry("TEST.DS");
-        expect(isDsEntry(entry)).toBe(true);
+        expect(FsDatasetsUtils.isDsEntry(entry)).toBe(true);
     });
 
     it("returns false if value is not a DsEntry", () => {
         const pds = new PdsEntry("TEST.PDS");
-        expect(isDsEntry(pds)).toBe(false);
+        expect(FsDatasetsUtils.isDsEntry(pds)).toBe(false);
     });
 });
 
 describe("isMemberEntry", () => {
     it("returns true if value is a MemberEntry", () => {
         const entry = new MemberEntry("TESTMEMB");
-        expect(isMemberEntry(entry)).toBe(true);
+        expect(FsDatasetsUtils.isMemberEntry(entry)).toBe(true);
     });
 
     it("returns false if value is not a MemberEntry", () => {
         const pds = new PdsEntry("TEST.PDS");
-        expect(isMemberEntry(pds)).toBe(false);
+        expect(FsDatasetsUtils.isMemberEntry(pds)).toBe(false);
     });
 });
 
 describe("isPdsEntry", () => {
     it("returns true if value is a PdsEntry", () => {
         const spoolEntry = new PdsEntry("TESTJOB.TEST.SPOOL.JES");
-        expect(isPdsEntry(spoolEntry)).toBe(true);
+        expect(FsDatasetsUtils.isPdsEntry(spoolEntry)).toBe(true);
     });
 
     it("returns false if value is not a PdsEntry", () => {
         const ds = new DsEntry("TEST.DS");
-        expect(isPdsEntry(ds)).toBe(false);
+        expect(FsDatasetsUtils.isPdsEntry(ds)).toBe(false);
     });
 });
