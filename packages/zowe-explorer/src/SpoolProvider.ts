5593020ac35d245c41e76c811ba2a02ed3c7210f
Timothy Johnson <timothy.johnson@broadcom.com>
2024-06-07 17:59:41 -0400

    Fix downloading spool files failing to match tree nodes
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- packages/zowe-explorer/src/SpoolProvider.ts
+++ packages/zowe-explorer/src/SpoolProvider.ts
@@ -15,6 +15,7 @@ import { ZoweExplorerApiRegister } from "./ZoweExplorerApiRegister";
 import { Profiles } from "./Profiles";
 import { ZoweLogger } from "./utils/LoggerUtils";
 import { IZoweJobTreeNode } from "@zowe/zowe-explorer-api";
+import type { ZoweSpoolNode } from "./job/ZoweJobNode";
 
 export default class SpoolProvider implements vscode.TextDocumentContentProvider {
     // Track files that have been opened previously through the SpoolProvider
@@ -145,10 +146,7 @@ export async function getSpoolFiles(node: IZoweJobTreeNode): Promise<zowe.IJobFi
  * @returns true if the selected node matches the spool file, false otherwise
  */
 export function matchSpool(spool: zowe.IJobFile, node: IZoweJobTreeNode): boolean {
-    return (
-        `${spool.stepname}:${spool.ddname} - ${spool["record-count"]}` === node.label.toString() ||
-        `${spool.stepname}:${spool.ddname} - ${spool.procstep}` === node.label.toString()
-    );
+    return spool.jobid === node.job?.jobid && spool.id === (node as ZoweSpoolNode).spool.id;
 }
 
 /**
