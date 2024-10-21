d6ddf6817cbbbd71bf607e4e4891c064aac64605
likhithanimma1 <142219673+likhithanimma1@users.noreply.github.com>
2024-10-22 00:11:24 +0530

    Address optional comments of #3162 (#3263)
    Signed-off-by: likhithanimma1 <142219673+likhithanimma1@users.noreply.github.com>
    Co-authored-by: Trae Yelovich <trae.yelovich@broadcom.com>

--- packages/zowe-explorer/src/webviews/src/table-view/ActionsBar.tsx
+++ packages/zowe-explorer/src/webviews/src/table-view/ActionsBar.tsx
@@ -81,8 +81,8 @@ export const ActionsBar = (props: ActionsProps) => {
       </h3>
       <span style={{ display: "flex", alignItems: "center", marginBottom: "0.25em" }}>
         <p style={{ fontSize: "0.9em", paddingTop: "2px", marginRight: "0.75em" }}>
-          {props.selectionCount === 0 ? l10n.t("No") : props.selectionCount} {l10n.t("item")}
-          {props.selectionCount > 1 || props.selectionCount === 0 ? l10n.t("s") : ""} {l10n.t("selected")}
+          {props.selectionCount === 0 ? l10n.t("No") : props.selectionCount}
+          {props.selectionCount > 1 || props.selectionCount === 0 ? l10n.t("items") : l10n.t("item")} {l10n.t("selected")}
         </p>
         {props.actions
           .filter((action) => (props.itemCount > 1 ? action.callback.typ === "multi-row" : action.callback.typ.endsWith("row")))
