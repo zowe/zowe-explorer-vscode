245f75ca668994b0227b8449b8febcd268cace39
Billie Simmons <BillieJean.Simmons@ibm.com>
2025-04-02 10:17:14 -0400

    Feature: creation of team config moved to ZE API and adopted by ZE (#3520)
    * initial work of moving code from ZE to ZE API for VSC creation of team config file
    
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
    
    * address ZE API unit test failure
    
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
    
    * remove Profiles tests that will move to ZE API for create team config
    
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
    
    * added CHANGELOG entries
    
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
    
    * Profiles.ts code cov
    
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
    
    * add some unit test cov
    
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
    
    * add additional createTeamConfiguration unit tests
    
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
    
    * add more codecov
    
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
    
    * adding more scenario coverage
    
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
    
    * remove some duplication in tests
    
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
    
    * Update packages/zowe-explorer/src/configuration/Profiles.ts
    
    Co-authored-by: Trae Yelovich <trae.yelovich@broadcom.com>
    Signed-off-by: Billie Simmons <BillieJean.Simmons@ibm.com>
    
    * remove vscode.l10n.t reference in ZE API & run prepublish
    
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Billie Simmons <49491949+JillieBeanSim@users.noreply.github.com>
    Signed-off-by: Billie Simmons <BillieJean.Simmons@ibm.com>
    Co-authored-by: Trae Yelovich <trae.yelovich@broadcom.com>

---
+++ packages/zowe-explorer-api/__mocks__/mockCreators/shared.ts
@@ -0,0 +1,94 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+import * as imperative from "@zowe/imperative";
+
+export function createConfigInstance() {
+    return {
+        load: jest.fn(),
+    } as any;
+}
+
+export function createConfigLoad() {
+    return {
+        configName: "zowe.config.json",
+        api: {
+            layers: {
+                merge: jest.fn(),
+                activate: jest.fn(),
+            },
+        },
+        layers: [
+            {
+                path: "file://globalPath/.zowe/zowe.config.json",
+                exists: true,
+                properties: undefined,
+                global: true,
+                user: false,
+            },
+            {
+                path: "file://projectPath/zowe.config.user.json",
+                exists: true,
+                properties: undefined,
+                global: false,
+                user: true,
+            },
+        ],
+        setSchema: jest.fn(),
+        save: jest.fn(),
+    } as any;
+}
+
+export function createTeamConfigMock(): imperative.IConfig {
+    return {
+        $schema: "./zowe.schema.json",
+        profiles: {
+            zosmf: {
+                type: "zosmf",
+                properties: {
+                    port: 443,
+                },
+                secure: [],
+            },
+            tso: {
+                type: "tso",
+                properties: {
+                    account: "",
+                    codePage: "1047",
+                    logonProcedure: "IZUFPROC",
+                },
+                secure: [],
+            },
+            ssh: {
+                type: "ssh",
+                properties: {
+                    port: 22,
+                },
+                secure: [],
+            },
+            base: {
+                type: "base",
+                properties: {
+                    host: "sample.com",
+                    rejectUnauthorized: true,
+                },
+                secure: ["user", "password"],
+            },
+        },
+        defaults: {
+            zosmf: "zosmf",
+            tso: "tso",
+            ssh: "ssh",
+            base: "base",
+        },
+        autoStore: true,
+    };
+}
