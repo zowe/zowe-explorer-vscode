b16176925cc32a0a20a25bd1afbdc0bd0b9928b4
Peter Haumer <4391934+phaumer@users.noreply.github.com>
2025-04-08 10:59:33 -0700

    Rewrite for style consistency
    Signed-off-by: Peter Haumer <4391934+phaumer@users.noreply.github.com>

--- packages/zowe-explorer/scripts/downloadLocalizations.js
+++ packages/zowe-explorer/scripts/downloadLocalizations.js
@@ -9,50 +9,66 @@
  *
  */
 
-/**
- * Downloads the latest translations submitted to Artifactory to
- * https://zowe.jfrog.io/ui/repos/tree/General/libs-snapshot-local/org/zowe/vscode/zowe-explorer-g11n
- * and copies them into the provides them into the top-level and l10n for building a localized extension.
- */
-
 const path = require("path");
 const fs = require("fs");
 const process = require("process");
 const tmp = require("tmp");
 const admZip = require("adm-zip");
 
-const artifactPath = "libs-snapshot-local/org/zowe/vscode/zowe-explorer-g11n";
-const baseUrl = new URL("https://zowe.jfrog.io");
-const getLatestInfoApiUrl = new URL(`artifactory/api/storage/${artifactPath}?lastModified`, baseUrl);
-
+/**
+ * Downloads the latest translations submitted to Artifactory to
+ * https://zowe.jfrog.io/ui/repos/tree/General/libs-snapshot-local/org/zowe/vscode/zowe-explorer-g11n
+ * and copies them into the provides them into the top-level and l10n for building a localized extension.
+ */
 (async () => {
-    const listResponse = await fetch(getLatestInfoApiUrl)
-        .then((r) => r.json())
-        .catch((error) => {
-            // Don't want to break builds if this does not work
-            console.warn(`WARNING. Error contacting Artifactory. Error: ${error}. Zowe Explorer build will not be localized.`);
-            process.exit(0);
-        });
-    const downloadUrl = new URL(listResponse.uri.replace("api/storage/", ""));
-    console.log(`Retrieving translations zip from ${new Date(listResponse.lastModified)} from Artifactory at ${downloadUrl}`);
+    const artifactPath = "libs-snapshot-local/org/zowe/vscode/zowe-explorer-g11n";
+    const baseUrl = new URL("https://zowe.jfrog.io");
+    const getLatestInfoApiUrl = new URL(`artifactory/api/storage/${artifactPath}?lastModified`, baseUrl);
 
-    fetch(downloadUrl)
-        .then((response) => response.arrayBuffer())
-        .then((arrayBuffer) => {
-            const tempFile = tmp.fileSync({ postfix: ".zip" }).name;
-            console.log(`Writing zip file to ${tempFile}`);
-            fs.writeFileSync(tempFile, Buffer.from(arrayBuffer));
-
-            // Files inside the zip are relative to top-level Zowe directory
-            const targetDir = path.normalize(path.join(__dirname, "../../.."));
-            console.log(`Extracting zip file to ${targetDir}`);
-            const zip = new admZip(tempFile);
-            zip.extractAllTo(targetDir, true);
-            fs.unlinkSync(tempFile);
-            console.log("Unzipped and removed zip file successfully.");
-        })
-        .catch((error) => {
-            console.warn(`WARNING. Error retrieving or unzipping translations zip file. Error: ${error}. Zowe Explorer build will not be localized.`);
-            process.exit(0);
-        });
+    const downloadUrl = await getLatestVersionInfo(getLatestInfoApiUrl);
+    const zipFilePath = await downloadArtifact(downloadUrl);
+    await unzipArtifact(zipFilePath);
 })();
+
+async function getLatestVersionInfo(getLatestInfoApiUrl) {
+    try {
+        const listResponse = await fetch(getLatestInfoApiUrl);
+        const listData = await listResponse.json();
+        const downloadUrl = new URL(listData.uri.replace("api/storage/", ""));
+        console.log(`Retrieving translations zip from ${new Date(listData.lastModified)} from Artifactory at ${downloadUrl}`);
+        return downloadUrl;
+    } catch (error) {
+        // Don't want to break builds if this does not work
+        console.warn(`WARNING. Error contacting Artifactory. Error: ${error}. Zowe Explorer build will not be localized.`);
+        process.exit(0);
+    }
+}
+
+async function downloadArtifact(downloadUrl) {
+    try {
+        const zipFileResponse = await fetch(downloadUrl);
+        const zipFileData = Buffer.from(await zipFileResponse.arrayBuffer());
+        const zipFilePath = tmp.fileSync({ postfix: ".zip" }).name;
+        console.log(`Writing zip file to ${zipFilePath}`);
+        fs.writeFileSync(zipFilePath, zipFileData);
+        return zipFilePath;
+    } catch (error) {
+        console.warn(`WARNING. Error retrieving translations zip file. Error: ${error}. Zowe Explorer build will not be localized.`);
+        process.exit(0);
+    }
+}
+
+async function unzipArtifact(zipFilePath) {
+    try {
+        // Files inside the zip are relative to top-level Zowe directory
+        const targetDir = path.normalize(path.join(__dirname, "../../.."));
+        console.log(`Extracting zip file to ${targetDir}`);
+        const zip = new admZip(zipFilePath);
+        zip.extractAllTo(targetDir, true);
+        fs.unlinkSync(zipFilePath);
+        console.log("Unzipped and removed zip file successfully.");
+    } catch (error) {
+        console.warn(`WARNING. Error unzipping translations zip file. Error: ${error}. Zowe Explorer build will not be localized.`);
+        process.exit(0);
+    }
+}
