dcf2268caf3642b964296da860dabaa5114ac0d9
Santhoshi Boyina <Santhoshi.Boyina1@ibm.com>
2025-04-11 14:53:33 +0530

    To check for ZoweJobNode tooltip containing jobID
    Signed-off-by: Santhoshi Boyina <Santhoshi.Boyina1@ibm.com>

--- packages/zowe-explorer/__tests__/__unit__/utils/AuthUtils.unit.test.ts
+++ packages/zowe-explorer/__tests__/__unit__/utils/AuthUtils.unit.test.ts
@@ -592,5 +592,31 @@ describe("AuthUtils", () => {
             expect(sessionNode.tooltip).toContain(`${sessionNode.description}`);
             expect(sessionNode.tooltip).toContain("Auth Method: Token-based Authentication");
         });
+
+        it("To check for ZoweJobNode tooltip containing jobID", async () => {
+            const sessionNode = createDatasetSessionNode(createISession(), serviceProfile);
+            const getChildrenSpy = jest.spyOn(sessionNode, "getChildren").mockResolvedValueOnce([]);
+            const refreshElementMock = jest.fn();
+            jest.spyOn(SharedTreeProviders, "getProviderForNode").mockReturnValueOnce({
+                refreshElement: refreshElementMock,
+            } as any);
+            const getSessionMock = jest.fn().mockReturnValue(createISession());
+            const sessionForProfile = (_profile) =>
+                ({
+                    getSession: getSessionMock,
+                } as any);
+            loadNamedProfileMock.mockClear().mockReturnValue(createIProfile());
+
+            jest.spyOn(AuthUtils, "isUsingTokenAuth").mockResolvedValueOnce(true);
+            sessionNode.description = "JobId: JOB0001";
+            await AuthUtils.syncSessionNode(sessionForProfile, sessionNode, sessionNode);
+            expect(getSessionMock).toHaveBeenCalled();
+            expect(sessionNode.dirty).toBe(true);
+            // await the promise since its result is discarded in the called function
+            await getChildrenSpy;
+            expect(getChildrenSpy).toHaveBeenCalled();
+            expect(sessionNode.tooltip).toContain(`${sessionNode.description}`);
+            expect(sessionNode.tooltip).toContain("Auth Method: Token-based Authentication");
+        });
     });
 });
