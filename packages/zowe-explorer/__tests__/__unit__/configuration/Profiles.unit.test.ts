d87b78ed18b1b92a8a46cef3b8aff9427749addd
Santhoshi Boyina <Santhoshi.Boyina1@ibm.com>
2025-04-14 15:07:11 +0530

    To check updated autoStore value
    Signed-off-by: Santhoshi Boyina <Santhoshi.Boyina1@ibm.com>

--- packages/zowe-explorer/__tests__/__unit__/configuration/Profiles.unit.test.ts
+++ packages/zowe-explorer/__tests__/__unit__/configuration/Profiles.unit.test.ts
@@ -977,6 +977,40 @@ describe("Profiles Unit Tests - function checkCurrentProfile", () => {
         jest.spyOn(Profiles.getInstance(), "validateProfiles").mockResolvedValue({ status: "active", name: "sestest" });
         await expect(Profiles.getInstance().checkCurrentProfile(testProfile)).resolves.toEqual({ name: "sestest", status: "active" });
     });
+
+    it("To check updated autoStore value", async () => {
+        const globalMocks = createGlobalMocks();
+        environmentSetup(globalMocks);
+        setupProfilesCheck(globalMocks);
+        const testNode = new (ZoweTreeNode as any)(
+            "test",
+            vscode.TreeItemCollapsibleState.None,
+            undefined,
+            globalMocks.testSession,
+            globalMocks.testProfile
+        );
+        jest.spyOn(Profiles.getInstance(), "validateProfiles").mockResolvedValue({ status: "active", name: "sestest" });
+        jest.spyOn(Profiles.getInstance(), "getProfileInfo").mockResolvedValue({
+            getTeamConfig: () => ({
+                properties: {
+                    autoStore: false,
+                },
+                set: jest.fn(),
+                delete: jest.fn(),
+                save: jest.fn(),
+            }),
+            getAllProfiles: jest.fn(),
+            mergeArgsForProfile: jest.fn(),
+        } as any);
+        const promptCredentialsSpy = jest.spyOn(Profiles.getInstance(), "promptCredentials").mockResolvedValueOnce(["sestest", "12345"]);
+        testNode.tooltip = "Auto Store: true\n";
+        await expect(Profiles.getInstance().checkCurrentProfile(globalMocks.testProfile, testNode)).resolves.toEqual({
+            name: "sestest",
+            status: "active",
+        });
+        expect(promptCredentialsSpy).toHaveBeenCalledTimes(1);
+        expect(testNode.tooltip).toContain("Auto Store: false");
+    });
 });
 
 describe("Profiles Unit Tests - function getProfileSetting", () => {
