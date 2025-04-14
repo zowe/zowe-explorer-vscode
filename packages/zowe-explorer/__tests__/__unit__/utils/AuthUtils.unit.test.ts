c757afb637933390160c5e3e68068b35d88a10c4
Santhoshi Boyina <Santhoshi.Boyina1@ibm.com>
2025-04-14 12:44:10 +0530

    To check for ZoweJobNode tooltip when job id is updated
    Signed-off-by: Santhoshi Boyina <Santhoshi.Boyina1@ibm.com>

--- packages/zowe-explorer/__tests__/__unit__/utils/AuthUtils.unit.test.ts
+++ packages/zowe-explorer/__tests__/__unit__/utils/AuthUtils.unit.test.ts
@@ -901,5 +901,32 @@ describe("AuthUtils", () => {
             expect(sessionNode.tooltip).toContain("Owner: * | Prefix: * | Status: ACTIVE");
             expect(sessionNode.tooltip).toContain("Auth Method: Token-based Authentication");
         });
+
+        it("To check for ZoweJobNode tooltip when job id is updated", async () => {
+            const sessionNode = createDatasetSessionNode(createISession(), serviceProfile);
+            const getChildrenSpy = jest.spyOn(sessionNode, "getChildren").mockResolvedValueOnce([]);
+            const refreshElementMock = jest.fn();
+            jest.spyOn(SharedTreeProviders, "getProviderForNode").mockReturnValueOnce({
+                refreshElement: refreshElementMock,
+            } as any);
+            const getSessionMock = jest.fn().mockReturnValue(createISession());
+            const sessionForProfile = (_profile) =>
+                ({
+                    getSession: getSessionMock,
+                } as any);
+            loadNamedProfileMock.mockClear().mockReturnValue(createIProfile());
+
+            jest.spyOn(AuthUtils, "isUsingTokenAuth").mockResolvedValueOnce(true);
+            sessionNode.tooltip = `Auth Method: Token Authentication\nJobId: JOB0001`;
+            sessionNode.description = "JobId: JOB0002";
+            await AuthUtils.syncSessionNode(sessionForProfile, sessionNode, sessionNode);
+            expect(getSessionMock).toHaveBeenCalled();
+            expect(sessionNode.dirty).toBe(true);
+            // await the promise since its result is discarded in the called function
+            await getChildrenSpy;
+            expect(getChildrenSpy).toHaveBeenCalled();
+            expect(sessionNode.tooltip).toContain("JobId: JOB0002");
+            expect(sessionNode.tooltip).toContain("Auth Method: Token-based Authentication");
+        });
     });
 });
