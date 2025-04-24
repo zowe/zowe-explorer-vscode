a934dd2264434093a33421b0a32b27d28cf59cf8
Santhoshi Boyina <Santhoshi.Boyina1@ibm.com>
2025-04-24 17:28:48 +0530

    refactor: To add a check for node tooltip
    Signed-off-by: Santhoshi Boyina <Santhoshi.Boyina1@ibm.com>

--- packages/zowe-explorer/src/trees/dataset/ZoweDatasetNode.ts
+++ packages/zowe-explorer/src/trees/dataset/ZoweDatasetNode.ts
@@ -910,7 +910,7 @@ export class ZoweDatasetNode extends ZoweTreeNode implements IZoweDatasetTreeNod
             }
             patternChanged = this.prevPattern !== finalPattern || this.pattern !== finalPattern;
             this.pattern = this.prevPattern = finalPattern;
-            const toolTipList: string[] = (this.tooltip as string).split("\n");
+            const toolTipList: string[] = (this.tooltip as string)?.split("\n") ?? [];
             const patternIndex = toolTipList.findIndex((key) => key.startsWith(vscode.l10n.t("Pattern: ")));
             if (patternIndex === -1) {
                 toolTipList.push(`${vscode.l10n.t("Pattern: ")}${this.pattern}`);
