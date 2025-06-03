3d1aca5c761388ed625525172e5c00884c1847ea
jace-roell <jace.roell@hotmail.com>
2025-06-03 15:00:44 -0400

    add top level describe for mocking profiles
    Signed-off-by: jace-roell <jace.roell@hotmail.com>

--- packages/zowe-explorer/src/trees/dataset/DatasetFSProvider.ts
+++ packages/zowe-explorer/src/trees/dataset/DatasetFSProvider.ts
@@ -512,7 +512,7 @@ export class DatasetFSProvider extends BaseProvider implements vscode.FileSystem
         // If the extenderTypeReady map does not contain the profile, create a deferred promise for the profile.
         const profileName = uri.path.split("/")[1];
         const profileInfo = Profiles.getInstance();
-        const profile = profileInfo?.allProfiles?.find((prof) => prof.name === profileName);
+        const profile = profileInfo.allProfiles.find((prof) => prof.name === profileName);
         if (profile && profile.type !== "zosmf" && !Profiles.extenderTypeReady.get(profileName)) {
             const deferredPromise = new DeferredPromise<void>();
             Profiles.extenderTypeReady.set(profileName, deferredPromise);
