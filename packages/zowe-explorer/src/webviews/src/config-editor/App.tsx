1f699801857324659d9f601db171d71c3aed0dd5
jace-roell <jace.roell@hotmail.com>
2025-06-03 10:03:52 -0400

    add clear button to dropdown input to avoid keyboard usage requirement
    Signed-off-by: jace-roell <jace.roell@hotmail.com>

--- packages/zowe-explorer/src/webviews/src/config-editor/App.tsx
+++ packages/zowe-explorer/src/webviews/src/config-editor/App.tsx
@@ -716,7 +716,7 @@ export function App() {
     <div className="modal-backdrop">
       <div className="modal">
         <h3>{l10n.t("Add New Profile Property")}</h3>
-        <div className="dropdown-container">
+        <div className="dropdown-container" style={{ position: "relative" }}>
           <input
             id="profile-type-input"
             value={newProfileKey}
@@ -728,7 +728,19 @@ export function App() {
             onBlur={() => setTimeout(() => setShowDropdown(false), 100)}
             className="modal-input"
             placeholder={l10n.t("New Key")}
+            style={{ paddingRight: "2rem" }}
           />
+          {newProfileKey && (
+            <button onClick={() => setNewProfileKey("")} className="profile-clear-button" title="Clear input">
+              <span
+                className="codicon codicon-chrome-close"
+                style={{
+                  fontSize: "14px",
+                  lineHeight: 1,
+                }}
+              />
+            </button>
+          )}
           {showDropdown && (
             <ul className="dropdown-list">
               {fetchTypeOptions(newProfileKeyPath || [])
@@ -748,6 +760,7 @@ export function App() {
             </ul>
           )}
         </div>
+
         <input
           placeholder={l10n.t("Value")}
           value={newProfileValue}
