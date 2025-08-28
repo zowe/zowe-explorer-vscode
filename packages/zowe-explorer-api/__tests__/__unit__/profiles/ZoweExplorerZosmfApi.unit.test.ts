a4cae186f57ba85261c0310240bb0921c2f164d2
Timothy Johnson <timothy.johnson@broadcom.com>
2025-08-28 16:52:55 -0400

    Add test for submitJcl with encoding
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- packages/zowe-explorer-api/__tests__/__unit__/profiles/ZoweExplorerZosmfApi.unit.test.ts
+++ packages/zowe-explorer-api/__tests__/__unit__/profiles/ZoweExplorerZosmfApi.unit.test.ts
@@ -666,7 +666,7 @@ describe("ZosmfJesApi", () => {
         {
             name: "submitJcl",
             spy: jest.spyOn(zosjobs.SubmitJobs, "submitJcl"),
-            args: ["jcl", "FB", "80"],
+            args: ["jcl", "FB", "80", undefined],
         },
         {
             name: "submitJob",
@@ -718,6 +718,20 @@ describe("ZosmfJesApi", () => {
                     retcode: "ACTIVE",
                 } as zosjobs.IJob)
             ).toBe(false);
+        });
+    });
+
+    describe("submitJcl", () => {
+        it("uses job encoding if specified in profile", async () => {
+            const api = new ZoweExplorerZosmf.JesApi();
+            api.profile = {
+                profile: {
+                    jobEncoding: "IBM-1147",
+                },
+            } as unknown as imperative.IProfileLoaded;
+            const submitJclSpy = jest.spyOn(zosjobs.SubmitJobs, "submitJcl");
+            await api.submitJcl("iefbr14");
+            expect(submitJclSpy).toHaveBeenLastCalledWith(undefined, "iefbr14", undefined, undefined, "IBM-1147");
         });
     });
 });
