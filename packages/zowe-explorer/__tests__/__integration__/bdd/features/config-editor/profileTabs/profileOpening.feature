a8db882d4bd18c4d23fde7741425a29bfe19d47f
Jace Roell <jace.roell@hotmail.com>
2025-09-23 23:51:11 -0400

    implement tab functions
    Signed-off-by: Jace Roell <jace.roell@hotmail.com>

--- packages/zowe-explorer/__tests__/__integration__/bdd/features/config-editor/profileTabs/profileOpening.feature
+++ packages/zowe-explorer/__tests__/__integration__/bdd/features/config-editor/profileTabs/profileOpening.feature
@@ -6,16 +6,15 @@ Background:
     And the profile list is set to flat view mode
 
 Scenario: Test profile header buttons
+    When a user right clicks a configuration tab and clicks open schema
+    Then a new file should be opened
+    Then close the current tab
     When a user right clicks a configuration tab and clicks open file
-    Then "zowe.config.json" should be the opened editor
-
-# Test 1:
-# Open config editor
-# Right Click a tab (div class="tab")
-# From the menu that appears click "open file"
-# the selected tab should be "zowe.config.json"
-# Clean up by closing the zowe.config.tab
-# Right click a tab
-# From menu select "open schema"
-# the selected tab should be "zowe.schema.json"
-# Clean up by closing the "zowe.schema.json" tab
+    Then a new file should be opened
+    Then close the current tab
+    When a user right clicks a configuration tab and clicks toggle autostore
+    And the user saves the changes
+    Then autostore should be "false"
+    When a user right clicks a configuration tab and clicks toggle autostore
+    And the user saves the changes
+    Then autostore should be "true"
--- packages/zowe-explorer/__tests__/__integration__/bdd/step_definitions/config-editor/profileOpening.steps.ts
+++ packages/zowe-explorer/__tests__/__integration__/bdd/step_definitions/config-editor/profileOpening.steps.ts
@@ -1,13 +1,64 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
 import { When, Then } from "@cucumber/cucumber";
+import { Key } from "webdriverio";
+import * as fs from "fs";
+import * as path from "path";
 
 When("a user right clicks a configuration tab and clicks open file", async () => {
-    const tab = await browser.$(`[id="global:true,user:false"]`);
+    const tab = await browser.$(`.tab`);
+    await tab.waitForExist({ timeout: 1000 });
     await tab.click({ button: "right" });
 
     const openFile = await browser.$(`[id="tab-open-file"]`);
+    await openFile.waitForExist({ timeout: 1000 });
     await openFile.click({ button: "left" });
 });
 
-Then("{string} should be the opened editor", async (expectedFile: string) => {
-    // Check tabs
+When("a user right clicks a configuration tab and clicks open schema", async () => {
+    const tab = await browser.$(`.tab`);
+    await tab.waitForExist({ timeout: 1000 });
+    await tab.click({ button: "right" });
+
+    const openFile = await browser.$(`[id="tab-open-schema"]`);
+    await openFile.waitForExist({ timeout: 1000 });
+    await openFile.click({ button: "left" });
+});
+
+Then("a new file should be opened", async () => {
+    const editorTabs = await browser.$$(".tab");
+    await expect(editorTabs.length).toBe(2);
+});
+Then("close the current tab", async () => {
+    await browser.pause(500);
+    await browser.keys([Key.Ctrl, "w"]);
+});
+
+When("a user right clicks a configuration tab and clicks toggle autostore", async () => {
+    const tab = await browser.$(`.tab`);
+    await tab.waitForExist({ timeout: 1000 });
+    await tab.click({ button: "right" });
+
+    const openFile = await browser.$(`[id="tab-toggle-autostore"]`);
+    await openFile.waitForExist({ timeout: 5000 });
+    await openFile.click({ button: "left" });
+    browser.keys(Key.Escape);
+});
+
+Then("autostore should be {string}", async (value: string) => {
+    const configPath = path.join(process.cwd(), "..", "ci", "zowe.config.json");
+    const configContent = fs.readFileSync(configPath, "utf8");
+    const config = JSON.parse(configContent);
+
+    const expectedValue = value === "true";
+    await expect(config.autoStore).toEqual(expectedValue);
 });
