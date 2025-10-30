b3b532c70e6ada7767c7d7c6da7cf7d6a8e19e54
Pujal <pujal.gandhi@broadcom.com>
2025-10-30 10:13:04 -0400

    added errorCode mock
    Signed-off-by: Pujal <pujal.gandhi@broadcom.com>

--- packages/zowe-explorer/__tests__/__unit__/trees/dataset/ZoweDatasetNode.unit.test.ts
+++ packages/zowe-explorer/__tests__/__unit__/trees/dataset/ZoweDatasetNode.unit.test.ts
@@ -1921,7 +1921,7 @@ describe("ZoweDatasetNode Unit Tests - listMembersInRange()", () => {
         jest.spyOn(pdsNode, "listMembers").mockRejectedValue(
             new imperative.ImperativeError({
                 msg: "Dataset not cataloged",
-                errorCode: "404",
+                errorCode: `${imperative.RestConstants.HTTP_STATUS_404}`,
             })
         );
 
