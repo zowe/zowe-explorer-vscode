a69d3bbc759af723b7020c17cd31ff227c1a4e3b
Timothy Johnson <timothy.johnson@broadcom.com>
2026-05-28 14:53:23 -0400

    Bundle server pax in ZE extension
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- packages/zowe-explorer/scripts/getSecretsPrebuilds.js
+++
@@ -1,4 +0,0 @@
-const { copySync } = require("fs-extra");
-const { join, resolve } = require("path");
-const secretsPkgDir = resolve(require.resolve("@zowe/secrets-for-zowe-sdk"), "..", "..");
-copySync(join(secretsPkgDir, "prebuilds"), resolve(__dirname, "..", "prebuilds"));
