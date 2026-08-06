c1bb5ad996edda98e3aa403b0e7d5dd9205fb417
CBforZ <chris.boehm@broadcom.com>
2026-08-06 14:47:14 -0400

    add available space check + prompt to warn user if it's less than the recommended space
    Signed-off-by: CBforZ <chris.boehm@broadcom.com>

--- packages/zowex-for-zowe-explorer/src/ServerDeployment.ts
+++ packages/zowex-for-zowe-explorer/src/ServerDeployment.ts
@@ -1,5 +1,5 @@
 import type { SshSession } from "@zowe/zos-uss-for-zowe-sdk";
-import { Gui, ZoweExplorerApiType } from "@zowe/zowe-explorer-api";
+import { Gui, imperative, ZoweExplorerApiType } from "@zowe/zowe-explorer-api";
 import * as vscode from "vscode";
 import { SshErrorHandler } from "./SshErrorHandler";
 import { ZSshUtils } from "@zowe/zowex-for-zowe-sdk";
@@ -19,6 +19,29 @@ export function deployWithProgress(session: SshSession, serverPath: string): The
                     progress.report({ increment: progressIncrement });
                 },
                 onError: errorCallback,
+                onInsufficientSpaceWarning: async (remainingMB: number, recommendedMB: number) => {
+                    const deployButton = vscode.l10n.t("Deploy");
+                    const cancelButton = vscode.l10n.t("Cancel");
+                    const message = vscode.l10n.t({
+                        message: "The remote directory {0} appears to only have {1} MB available," +
+                            " less than the recommended {2} MB of free space. Would you like to attempt deployment anyway?",
+                        args: [serverPath, remainingMB, recommendedMB],
+                        comment: ["The user-specified or default server path, available space " +
+                            "for the server path and recommended available space in megabytes"],
+                    });
+                    imperative.Logger.getAppLogger().info(`Prompting the user to determine whether we should proceed` +
+                        ` with the deployment despite the apparent lack of disk space (${remainingMB} of ${recommendedMB} MB)`);
+                    // Accessibility: Capture the button response to ensure screen readers properly announce
+                    // the buttons as actionable elements rather than just informational text.
+                    const selection = await Gui.showMessage(message, { items: [deployButton, cancelButton] });
+                    if (selection === deployButton) {
+                        imperative.Logger.getAppLogger().info("User accepted the risk of insufficient disk space");
+                        return true;
+                    } else {
+                        imperative.Logger.getAppLogger().info("User declined to continue with the deployment after disk space warning");
+                        return false;
+                    }
+                }
             });
         }
     );
