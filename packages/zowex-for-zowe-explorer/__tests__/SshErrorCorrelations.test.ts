4bccfa604ad9ad881725370a08bec6632936af46
jace-roell <jace.roell@hotmail.com>
2026-08-24 09:05:45 -0400

    fix test from new sdk version
    Signed-off-by: jace-roell <jace.roell@hotmail.com>

--- packages/zowex-for-zowe-explorer/__tests__/SshErrorCorrelations.test.ts
+++ packages/zowex-for-zowe-explorer/__tests__/SshErrorCorrelations.test.ts
@@ -387,6 +387,9 @@ describe("SshErrorCorrelations", () => {
                 "FOTS4150",
                 "FOTS4312",
                 "UNRECOGNIZED_COMMAND",
+                // Server error
+                "CEE3501S",
+                "CEE3561S",
             ];
 
             // Sanity check: the curated list should match the SDK's full set of registered codes.
